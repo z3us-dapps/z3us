67354cfd0110936e98471a0c2ab53169a82a3c1d
Z.God <zac.godinez@tutanota.com>
2022-04-29 21:47:49 +1000

    refactor: update to website config and references

--- apps/website/config/index.ts
+++ apps/website/config/index.ts
@@ -1,6 +1,7 @@
-export const urls = {
-	Z3US: 'https://www.z3us.com',
-	GITHUB: 'https://github.com/z3us-dapps',
-	TELEGRAM: 'https://t.me/+HknrTWQ1VAE5Njk5',
-	TWITTER: 'https://twitter.com/z3us_dapps',
+export const config = {
+	Z3US_URL: 'https://www.z3us.com',
+	GITHUB_URL: 'https://github.com/z3us-dapps',
+	TELEGRAM_URL: 'https://t.me/+HknrTWQ1VAE5Njk5',
+	TWITTER_URL: 'https://twitter.com/z3us_dapps',
+	TWITTER_HANDLE: 'z3us_dapps',
 }
