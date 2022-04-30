f71503de1d4cd9ca5ba65ebfc60d977492fb6e11
Heath Snee <heath.snee@gmail.com>
2022-04-30 22:36:14 +1000

    fix: disable staking with ads==no validators, indicate fee by colour

--- apps/extension/src/containers/wallet-panel/staking/validator-item/index.tsx
+++ apps/extension/src/containers/wallet-panel/staking/validator-item/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Validator } from '@src/services/types'
-import { DownloadIcon, UploadIcon, ExternalLinkIcon, Cross2Icon } from '@radix-ui/react-icons'
+import { DownloadIcon, UploadIcon, InfoCircledIcon, Cross2Icon, ExternalLinkIcon } from '@radix-ui/react-icons'
 import { getShortAddress } from '@src/utils/string-utils'
 import { Box, Flex, Text } from 'ui/src/components/atoms'
 import { Popover, PopoverTrigger, PopoverContent, PopoverClose } from 'ui/src/components/popover'
@@ -38,118 +38,49 @@ export const ValidatorItem = ({ i, validator, totalStakes, style }: IProps): JSX
 		window.open(`${EXPLORER_URL}/validators/${address.toString()}`)
 	}
 
+	let backgroundColor
+	if (!isExternalStakeAccepted) {
+		backgroundColor = 'gray'
+	} else if (validatorFee >= 3) {
+		backgroundColor = 'red'
+	} else if (validatorFee >= 1.5) {
+		backgroundColor = 'orange'
+	}
+
 	return (
 		<div style={style}>
 			<Flex
 				align="center"
-				css={{ borderTop: `1px solid ${i === 0 ? 'transparent' : '$borderPanel'}`, px: '$3', py: '$2' }}
+				css={{
+					borderTop: `1px solid ${i === 0 ? 'transparent' : '$borderPanel'}`,
+					px: '$3',
+					py: '$2',
+					backgroundColor,
+				}}
 			>
 				<Box>
-					<Popover>
-						<PopoverTrigger asChild>
-							<Text
-								truncate
-								aria-label="Validator popover"
-								css={{ maxWidth: '175px', '&:hover': { textDecoration: 'underline', cursor: 'pointer' } }}
-							>
-								{name}
-							</Text>
-						</PopoverTrigger>
-						<PopoverContent css={{ width: '275px' }}>
-							<Box css={{ p: '$4' }}>
-								<Flex css={{ mb: '$1' }}>
-									<Text medium size="3" truncate css={{ width: LEFT_COL_WIDTH }}>
-										Validator information
-									</Text>
-								</Flex>
-								<Flex css={{ mt: '$2' }}>
-									<Text size="2" truncate css={{ width: LEFT_COL_WIDTH }}>
-										Name
-									</Text>
-									<Flex justify="end" css={{ flex: '1' }}>
-										<Text size="2" color="muted">
-											{name}
-										</Text>
-									</Flex>
-								</Flex>
-								<Flex css={{ mt: '$2' }}>
-									<Text size="2" truncate css={{ width: LEFT_COL_WIDTH }}>
-										Address
-									</Text>
-									<Flex justify="end" css={{ flex: '1' }}>
-										<Text size="2" color="muted">
-											{getShortAddress(address.toString())}
-										</Text>
-									</Flex>
-								</Flex>
-								<Flex css={{ mt: '$2' }}>
-									<Text size="2" truncate css={{ width: LEFT_COL_WIDTH }}>
-										Total / owner
-									</Text>
-									<Flex justify="end" css={{ flex: '1' }}>
-										<Text size="2" color="muted" css={{ textAlign: 'right' }}>
-											{formatBigNumber(total)} / {formatBigNumber(new BigNumber(ownerDelegation).shiftedBy(-18))}
-										</Text>
-									</Flex>
-								</Flex>
-								<Flex css={{ mt: '$2' }}>
-									<Text size="2" truncate css={{ width: LEFT_COL_WIDTH }}>
-										Share
-									</Text>
-									<Flex justify="end" css={{ flex: '1' }}>
-										<Text size="2" color="muted">
-											{validatorFee}%
-										</Text>
-									</Flex>
-								</Flex>
-								<Flex css={{ mt: '$2' }}>
-									<Text size="2" truncate css={{ width: LEFT_COL_WIDTH }}>
-										Uptime
-									</Text>
-									<Flex justify="end" css={{ flex: '1' }}>
-										<Text size="2" color="muted">
-											{uptimePercentage}%
-										</Text>
-									</Flex>
-								</Flex>
-								<Flex css={{ mt: '$2' }}>
-									<Text size="2" truncate css={{ width: LEFT_COL_WIDTH }}>
-										APY
-									</Text>
-									<Flex justify="end" css={{ flex: '1' }}>
-										<Text size="2" color="muted">
-											{apy(total, totalStakes, validatorFee).toFormat(2)}%
-										</Text>
-									</Flex>
-								</Flex>
-								<Flex css={{ mt: '$2' }}>
-									<Text size="2" truncate css={{ width: LEFT_COL_WIDTH }}>
-										ADS
-									</Text>
-									<Flex justify="end" css={{ flex: '1' }}>
-										<Text size="2" color="muted">
-											{isExternalStakeAccepted ? 'YES' : 'NO'}
-										</Text>
-									</Flex>
-								</Flex>
-							</Box>
-							<PopoverClose aria-label="Close popover">
-								<Cross2Icon />
-							</PopoverClose>
-						</PopoverContent>
-					</Popover>
+					<Text
+						onClick={handleGotoTokenExplorer}
+						truncate
+						aria-label="Validator popover"
+						css={{ maxWidth: '175px', '&:hover': { textDecoration: 'underline', cursor: 'pointer' } }}
+					>
+						{name} <ExternalLinkIcon />
+					</Text>
 				</Box>
 				<Flex justify="end" css={{ pl: '$2', flex: '1' }}>
 					<Flex align="center">
-						<StakeModal
-							validatorAddress={address.toString()}
-							tooltipMessage="Stake XRD"
-							trigger={
-								<Button color="ghost" iconOnly aria-label="Stake XRD with validator" size="1">
-									<UploadIcon />
-								</Button>
-							}
-						/>
+						{isExternalStakeAccepted && (
+							<StakeModal
+								validatorAddress={address.toString()}
+								tooltipMessage="Stake XRD"
+								trigger={
+									<Button color="ghost" iconOnly aria-label="Stake XRD with validator" size="1">
+										<UploadIcon />
+									</Button>
+								}
+							/>
+						)}
 						<StakeModal
 							validatorAddress={address.toString()}
 							reduceStake
@@ -162,9 +93,95 @@ export const ValidatorItem = ({ i, validator, totalStakes, style }: IProps): JSX
 						/>
 						<Tooltip>
 							<TooltipTrigger asChild>
-								<Button color="ghost" iconOnly aria-label="Unstake XRD" size="1" onClick={handleGotoTokenExplorer}>
-									<ExternalLinkIcon />
-								</Button>
+								<Popover>
+									<PopoverTrigger asChild>
+										<Button color="ghost" iconOnly aria-label="Unstake XRD" size="1">
+											<InfoCircledIcon />
+										</Button>
+									</PopoverTrigger>
+									<PopoverContent css={{ width: '275px' }}>
+										<Box css={{ p: '$4' }}>
+											<Flex css={{ mb: '$1' }}>
+												<Text medium size="3" truncate css={{ width: LEFT_COL_WIDTH }}>
+													Validator information
+												</Text>
+											</Flex>
+											<Flex css={{ mt: '$2' }}>
+												<Text size="2" truncate css={{ width: LEFT_COL_WIDTH }}>
+													Name
+												</Text>
+												<Flex justify="end" css={{ flex: '1' }}>
+													<Text size="2" color="muted">
+														{name}
+													</Text>
+												</Flex>
+											</Flex>
+											<Flex css={{ mt: '$2' }}>
+												<Text size="2" truncate css={{ width: LEFT_COL_WIDTH }}>
+													Address
+												</Text>
+												<Flex justify="end" css={{ flex: '1' }}>
+													<Text size="2" color="muted">
+														{getShortAddress(address.toString())}
+													</Text>
+												</Flex>
+											</Flex>
+											<Flex css={{ mt: '$2' }}>
+												<Text size="2" truncate css={{ width: LEFT_COL_WIDTH }}>
+													Total / owner
+												</Text>
+												<Flex justify="end" css={{ flex: '1' }}>
+													<Text size="2" color="muted" css={{ textAlign: 'right' }}>
+														{formatBigNumber(total)} / {formatBigNumber(new BigNumber(ownerDelegation).shiftedBy(-18))}
+													</Text>
+												</Flex>
+											</Flex>
+											<Flex css={{ mt: '$2' }}>
+												<Text size="2" truncate css={{ width: LEFT_COL_WIDTH }}>
+													Share
+												</Text>
+												<Flex justify="end" css={{ flex: '1' }}>
+													<Text size="2" color="muted">
+														{validatorFee}%
+													</Text>
+												</Flex>
+											</Flex>
+											<Flex css={{ mt: '$2' }}>
+												<Text size="2" truncate css={{ width: LEFT_COL_WIDTH }}>
+													Uptime
+												</Text>
+												<Flex justify="end" css={{ flex: '1' }}>
+													<Text size="2" color="muted">
+														{uptimePercentage}%
+													</Text>
+												</Flex>
+											</Flex>
+											<Flex css={{ mt: '$2' }}>
+												<Text size="2" truncate css={{ width: LEFT_COL_WIDTH }}>
+													APY
+												</Text>
+												<Flex justify="end" css={{ flex: '1' }}>
+													<Text size="2" color="muted">
+														{apy(total, totalStakes, validatorFee).toFormat(2)}%
+													</Text>
+												</Flex>
+											</Flex>
+											<Flex css={{ mt: '$2' }}>
+												<Text size="2" truncate css={{ width: LEFT_COL_WIDTH }}>
+													ADS
+												</Text>
+												<Flex justify="end" css={{ flex: '1' }}>
+													<Text size="2" color="muted">
+														{isExternalStakeAccepted ? 'YES' : 'NO'}
+													</Text>
+												</Flex>
+											</Flex>
+										</Box>
+										<PopoverClose aria-label="Close popover">
+											<Cross2Icon />
+										</PopoverClose>
+									</PopoverContent>
+								</Popover>
 							</TooltipTrigger>
 							<TooltipContent sideOffset={5} css={{ backgroundColor: '$bgPanel2' }}>
 								<TooltipArrow css={{ fill: '$bgPanel2' }} />
