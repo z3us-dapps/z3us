fc9cef44dcb8f9bd3d4b9cd23ad40e8f1c92d2df
Z.God <zac.godinez@tutanota.com>
2022-05-12 20:39:13 +1000

    style: updated the terms styles

--- apps/website/components/mdx-theme/index.tsx
+++ apps/website/components/mdx-theme/index.tsx
@@ -10,14 +10,43 @@ const components = {
 	Button,
 	ExampleV1,
 	SyntaxHighlighter,
-	h1: props => <Text bold size="12" css={{ py: '$5' }} {...props} />,
-	h2: props => <Text size="10" css={{ py: '$3' }} {...props} />,
-	h3: props => <Text size="8" css={{ py: '$3' }} {...props} />,
-	h4: props => <Text size="7" css={{ py: '$3' }} {...props} />,
-	h5: props => <Text size="5" css={{ py: '$3' }} {...props} />,
-	h6: props => <Text size="4" css={{ py: '$3' }} {...props} />,
-	p: props => <Text size="5" regular css={{ py: '$3', lineHeight: '24px' }} {...props} />,
-	a: props => <StyledLink bubble {...props} />,
+	h1: props => (
+		<Text
+			as="h1"
+			bold
+			size="12"
+			css={{
+				py: '$5',
+				fontFamily: '$HaasGrotDisplayRound',
+				fontSize: '26px',
+				lineHeight: '30px',
+
+				'@md': { fontSize: '36px', lineHeight: '46px' },
+			}}
+			{...props}
+		/>
+	),
+	h2: props => <Text as="h2" size="10" css={{ py: '$3' }} {...props} />,
+	h3: props => <Text as="h3" size="8" css={{ py: '$3' }} {...props} />,
+	h4: props => <Text as="h4" size="7" css={{ py: '$3' }} {...props} />,
+	h5: props => <Text as="h5" size="5" css={{ py: '$3' }} {...props} />,
+	h6: props => <Text as="h6" size="4" css={{ py: '$3' }} {...props} />,
+	p: props => (
+		<Text
+			as="p"
+			size="5"
+			regular
+			css={{
+				py: '$3',
+				fontFamily: '$HaasGrotTextRound',
+				fontSize: '15px',
+				lineHeight: '22px',
+				'@md': { fontSize: '16px', lineHeight: '26px' },
+			}}
+			{...props}
+		/>
+	),
+	a: props => <StyledLink as="a" bubble {...props} />,
 }
 
 interface IProps {
