63ab6aeda57d61286fe757415ee6037c16a1f2a6
Heath Snee <heath.snee@gmail.com>
2022-05-17 20:55:46 +1000

    fix: check error when adding notificatoin

--- apps/extension/src/hooks/use-watch-transactions.tsx
+++ apps/extension/src/hooks/use-watch-transactions.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-await-in-loop */
 import browser from 'webextension-polyfill'
 import { useEffect, useState } from 'react'
 import { useLastFiveTransactions } from '@src/services/react-query/queries/radix'
@@ -15,32 +16,43 @@ export const useWatchTransactions = () => {
 	const [lastTxIds, setLastTxIds] = useState({})
 
 	useEffect(() => {
-		if (isLoading) return
-		if (!transactionMap) return
+		if (isLoading) return () => {}
+		if (!transactionMap) return () => {}
+
+		let mounted = true
 
 		const newLastTxIds = {}
-		Object.keys(transactionMap).forEach(address => {
+		Object.keys(transactionMap).forEach(async address => {
 			const transactions = transactionMap[address]
-			const lastTxId = lastTxIds[address]
+			if (transactions.length > 0) {
+				newLastTxIds[address] = transactions[0].id
+			}
 
+			const lastTxId = lastTxIds[address]
 			if (lastTxId) {
 				for (let i = 0; i < transactions.length; i += 1) {
 					if (lastTxId === transactions[i].id) {
 						break
 					}
-					browser.notifications.create(transactions[i].id, {
+					if (!mounted) return
+					await browser.notifications.create(transactions[i].id, {
 						title,
 						type: 'basic',
 						message: 'There is a new transaction on your account',
 						iconUrl: browser.runtime.getURL('favicon-128x128.png'),
 					})
+					const { lastError } = browser.runtime
+					if (lastError) {
+						console.error(lastError)
+					}
 				}
-			}
-			if (transactions.length > 0) {
-				newLastTxIds[address] = transactions[0].id
 			}
 		})
 
 		setLastTxIds(newLastTxIds)
+
+		return () => {
+			mounted = false
+		}
 	}, [isLoading, transactionMap])
 }
