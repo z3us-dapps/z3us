d1f6f520354e9fb86c15ecb2202cd908a111d758
Heath Snee <heath.snee@gmail.com>
2022-07-11 16:20:31 +1000

    fix: reconnect port on disconnect

--- apps/extension/src/lib/content-script.ts
+++ apps/extension/src/lib/content-script.ts
@@ -2,16 +2,41 @@ import browser from 'webextension-polyfill'
 // import init from 'pte-manifest-compiler'
 import { PORT_NAME, TARGET_BACKGROUND, TARGET_INPAGE } from '../services/messanger'
 
-let port = browser.runtime.connect({ name: PORT_NAME })
+const connectNewPort = () => {
+	const port = browser.runtime.connect({ name: PORT_NAME })
+	port.onMessage.addListener(message => {
+		if (message.target !== TARGET_INPAGE) {
+			return
+		}
 
-port.onDisconnect.addListener(() => {
-	if (port.error) {
-		// eslint-disable-next-line no-console
-		console.error(`Disconnected due to an error: ${port.error.message}`)
+		window.postMessage({ ...message, source: TARGET_BACKGROUND })
+	})
+
+	const listener = event => {
+		if (event.source !== window) {
+			return
+		}
+		const message = event.data
+		if (message.target !== TARGET_BACKGROUND) {
+			return
+		}
+
+		port.postMessage({ ...message, source: TARGET_INPAGE })
 	}
-	console.warn('Disconnected, reconnecting...')
-	port = browser.runtime.connect({ name: PORT_NAME })
-})
+
+	port.onDisconnect.addListener(() => {
+		if (port.error) {
+			// eslint-disable-next-line no-console
+			console.error(`Disconnected due to an error: ${port.error.message}`)
+		}
+		window.removeEventListener('message', listener, false)
+		connectNewPort()
+	})
+
+	window.addEventListener('message', listener, false)
+}
+
+connectNewPort()
 
 const script = document.createElement('script')
 script.type = 'module'
@@ -23,27 +48,3 @@ head.appendChild(script)
 // const wasmPath = browser.runtime.getURL('pte_manifest_compiler_bg.wasm')
 // // eslint-disable-next-line no-console
 // init(wasmPath).catch(console.error)
-
-port.onMessage.addListener(message => {
-	if (message.target !== TARGET_INPAGE) {
-		return
-	}
-
-	window.postMessage({ ...message, source: TARGET_BACKGROUND })
-})
-
-window.addEventListener(
-	'message',
-	event => {
-		if (event.source !== window) {
-			return
-		}
-		const message = event.data
-		if (message.target !== TARGET_BACKGROUND) {
-			return
-		}
-
-		port.postMessage({ ...message, source: TARGET_INPAGE })
-	},
-	false,
-)
