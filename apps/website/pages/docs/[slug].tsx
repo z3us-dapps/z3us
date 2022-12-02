82b74e65eee3d22dff711cb2f65346353c700558
Zac Godinez <zac.godinez@tutanota.com>
2022-12-03 08:17:48 +1100

    fix: work on the docs menu

--- apps/website/pages/docs/[slug].tsx
+++ apps/website/pages/docs/[slug].tsx
@@ -4,11 +4,17 @@ import { serialize } from 'next-mdx-remote/serialize'
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypePrettyCode from 'rehype-pretty-code'
 import { NextSeo } from 'next-seo'
 import { PageDocs } from 'components/pages/page-docs'
 import { config } from 'config'
+import { mdxOptions, getTableOfContents, getAllFiles } from '../../docs/utils'
 
-const PostPage = ({ docs, mdxSource }) => {
+const DOCS_FOLDER = 'docs'
+
+const PostPage = ({ docs, toc, mdxSource }) => {
 	return (
 		<>
 			<NextSeo
@@ -34,7 +40,7 @@ const PostPage = ({ docs, mdxSource }) => {
 					],
 				}}
 			/>
-			<PageDocs docs={docs} mdxSource={mdxSource} />
+			<PageDocs toc={toc} docs={docs} mdxSource={mdxSource} />
 		</>
 	)
 }
@@ -55,24 +61,27 @@ const getStaticPaths = async () => {
 
 const getStaticProps = async ({ params: { slug } }) => {
 	const files = fs.readdirSync(path.join('docs'))
-	const docs = files.map(filename => {
-		const markdownWithMeta = fs.readFileSync(path.join('docs', filename), 'utf-8')
-		const { data: frontMatter } = matter(markdownWithMeta)
-
-		return {
-			frontMatter,
-			slug: filename.split('.')[0],
-		}
-	})
+	const docs = getAllFiles(DOCS_FOLDER, [])
 	const markdownWithMeta = fs.readFileSync(path.join('docs', `${slug}.mdx`), 'utf-8')
 	const { data: frontMatter, content } = matter(markdownWithMeta)
-	const mdxSource = await serialize(content)
+	const toc = getTableOfContents(content)
+	// const { content } = matter(markdownWithMeta)
+	// const mdxSource = await serialize(content)
+
+	const mdxSource = await serialize(content, {
+		scope: {},
+		mdxOptions: {
+			remarkPlugins: [],
+			rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings, [rehypePrettyCode, mdxOptions]],
+			format: 'mdx',
+		},
+		parseFrontmatter: false,
+	})
 
 	return {
 		props: {
 			docs,
-			frontMatter,
-			slug,
+			toc,
 			mdxSource,
 		},
 	}
--- apps/website/pages/docs/index.tsx
+++ apps/website/pages/docs/index.tsx
@@ -2,7 +2,6 @@ import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 import { serialize } from 'next-mdx-remote/serialize'
-import { slug as githubSlugger } from 'github-slugger'
 import rehypeSlug from 'rehype-slug'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypePrettyCode from 'rehype-pretty-code'
@@ -11,133 +10,9 @@ import { NextSeo } from 'next-seo'
 import { PageDocs } from 'components/pages/page-docs'
 import { config } from 'config'
 import { DocsPageProps } from 'types'
+import { mdxOptions, getTableOfContents, getAllFiles } from '../../docs/utils'
 
-const options = {
-  // theme: 'one-dark-pro',
-  // theme: 'github-light',
-  theme: {
-    dark: 'github-dark',
-    light: 'github-light',
-  },
-  onVisitLine(node) {
-    // Prevent lines from collapsing in `display: grid` mode, and
-    // allow empty lines to be copy/pasted
-    if (node.children.length === 0) {
-      node.children = [{ type: 'text', value: ' ' }]
-    }
-  },
-  // Feel free to add classNames that suit your docs
-  onVisitHighlightedLine(node) {
-    node.properties.className.push('highlighted')
-  },
-  onVisitHighlightedWord(node) {
-    node.properties.className = ['word']
-  },
-}
-
-const META = '_meta.json'
 const DOCS_FOLDER = 'docs'
-
-export const getTableOfContents = (content: any) => {
-  const regexp = new RegExp(/^(### |## )(.*)\n/, 'gm')
-  const headings = [...content.matchAll(regexp)]
-
-  let tableOfContents = []
-
-  if (headings.length) {
-    tableOfContents = headings.map(heading => {
-      const headingText = heading[2].trim()
-      let headingType = heading[1].trim() === '##' ? 'h2' : 'h3'
-
-      switch (heading[1]?.trim()) {
-        case '##':
-          headingType = 'h2'
-          break
-        case '###':
-          headingType = 'h3'
-          break
-        case '####':
-          headingType = 'h4'
-          break
-        case '#####':
-          headingType = 'h5'
-          break
-        case 'Mangoes':
-          break
-        default:
-          break
-      }
-
-      const headingLink = githubSlugger(`z3-${headingText}`)
-
-      return {
-        title: headingText,
-        link: `#${headingLink}`,
-        headingType,
-      }
-    })
-  }
-
-  return tableOfContents
-}
-
-const getAllFiles = (dirPath: string, map: object = {}, folderPath: string = '') => {
-  const files = fs.readdirSync(dirPath)
-  let fileMap = {
-    ...map,
-  }
-  files.forEach(file => {
-    const p = `${dirPath}/${file}`
-    if (fs.statSync(p).isDirectory()) {
-      fileMap = {
-        ...fileMap,
-        ...getAllFiles(`${dirPath}/${file}`, fileMap, p),
-      }
-    } else if (file === META) {
-      const metaObj = JSON.parse(fs.readFileSync(path.join(p), 'utf-8'))
-      const isFileInFolder = folderPath.length > 0
-      const pathArr = (folderPath || '')
-        .replace(DOCS_FOLDER, '')
-        .split('/')
-        .filter(a => a !== '')
-      const pathLength = pathArr.length
-      const folderName = pathArr[pathArr.length - 1]
-      const folderParent = pathArr?.[pathArr.length - 2]
-      const menu = Object.entries(metaObj).reduce(
-        (acc, [slug, title]) => ({
-          ...acc,
-          [slug]: { title, slug },
-        }),
-        {},
-      )
-      fileMap = {
-        ...fileMap,
-        ...(isFileInFolder
-          ? {
-            ...(pathLength === 1
-              ? { [folderName]: { ...fileMap?.[folderName], ...menu } }
-              : {
-                ...{
-                  [folderParent]: {
-                    ...fileMap[folderParent],
-                    [folderName]: {
-                      ...fileMap[folderParent][folderName],
-                      ...menu,
-                    },
-                  },
-                },
-              }),
-          }
-          : { ...menu }),
-      }
-
-      // TODO, do we need this ?
-      // arrayOfFiles.push(path.join(__dirname, dirPath, '/', file))
-    }
-  })
-
-  return fileMap
-}
 
 export const DocsIndex: React.FC<DocsPageProps> = ({ docs, toc, mdxSource }) => (
   <>
@@ -177,7 +52,7 @@ export const getStaticProps = async () => {
     scope: {},
     mdxOptions: {
       remarkPlugins: [],
-      rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings, [rehypePrettyCode, options]],
+      rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings, [rehypePrettyCode, mdxOptions]],
       format: 'mdx',
     },
     parseFrontmatter: false,
