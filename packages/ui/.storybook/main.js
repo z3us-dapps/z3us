24235bdac7f1e65e46e2324947ee6da54ca926a2
Zac Godinez <zac.godinez@tutanota.com>
2022-12-14 07:01:10 +1100

    chore: implemented dark mode and screens for storybook

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,18 +1,17 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
-  },
-  "features": {
-    "storyStoreV7": true
-  }
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-viewport',
+		'storybook-dark-mode',
+	],
+	framework: '@storybook/react',
+	core: {
+		builder: '@storybook/builder-vite',
+	},
+	features: {
+		storyStoreV7: true,
+	},
 }
--- packages/ui/.storybook/preview.js
+++ packages/ui/.storybook/preview.js
@@ -1,9 +1,30 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+import { zDecorator } from './decorators'
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+	viewport: {
+		viewports: INITIAL_VIEWPORTS,
+	},
+	backgrounds: {
+		default: 'white',
+		values: [
+			{
+				name: 'white',
+				value: '#fff',
+			},
+		],
+	},
+	darkMode: {
+		// -  current: 'light',
+		current: 'dark',
+	},
 }
+
+export const decorators = [zDecorator]
