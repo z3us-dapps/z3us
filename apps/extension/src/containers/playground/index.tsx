3f9ffa18ca6bb35bd3c234f030b27ac6ee1b09fd
Zac Godinez <zac.godinez@tutanota.com>
2022-12-19 06:58:11 +1100

    feat: added playground for extension ui

--- apps/extension/src/containers/playground/index.tsx
+++ apps/extension/src/containers/playground/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-
 import { globalStyles } from 'ui/src/theme'
+
+import '@src/css/app.css'
 
 export const Playground: React.FC = () => {
 	globalStyles()
--- apps/extension/src/main-dev.tsx
+++ apps/extension/src/main-dev.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Playground from './containers/playground'
-import '@src/css/app.scss'
 
 const rootElement = document.getElementById('root')
 
 render(
 	<React.StrictMode>
-		<Playground/>
+		<Playground />
 	</React.StrictMode>,
 	rootElement,
 )
