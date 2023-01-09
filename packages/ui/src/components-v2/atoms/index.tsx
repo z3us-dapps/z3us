0b2e6c29e62cd608ee943d7c96134701908072ec
Zac Godinez <zac.godinez@tutanota.com>
2023-01-10 05:52:51 +1100

    style: added text atom

---
+++ packages/ui/src/components-v2/atoms/index.tsx
@@ -0,0 +1,4 @@
+import { Box } from './box'
+import { Paragraph, Text } from './text'
+
+export { Box, Paragraph, Text }
---
+++ packages/ui/src/components-v2/atoms/text.tsx
@@ -0,0 +1,58 @@
+import { cva, VariantProps } from 'class-variance-authority'
+import React from 'react'
+
+import './text.css'
+
+const cvaVariants = {
+	size: {
+		tiny: ['z3-c-text--tiny'],
+		xs: ['z3-c-text--xs'],
+		sm: ['z3-c-text--sm'],
+		base: ['z3-c-text--base'],
+		lg: ['z3-c-text--lg'],
+		xl: ['z3-c-text--xl'],
+		xl2: ['z3-c-text--xl2'],
+		xl3: ['z3-c-text--xl3'],
+		xl4: ['z3-c-text--xl4'],
+		xl5: ['z3-c-text--xl5'],
+		xl6: ['z3-c-text--xl6'],
+		xl7: ['z3-c-text--xl7'],
+	},
+	defaultLineHeight: {
+		true: ['z3-c-text--line-height'],
+		false: [''],
+	},
+	defaultLetterSpacing: {
+		true: ['z3-c-text--letter-spacing'],
+		false: [''],
+	},
+}
+
+const cvaDefaults = {
+	size: 'base',
+	defaultLineHeight: true,
+	defaultLetterSpacing: true,
+}
+
+export const cvaText = cva('z3-c-text', {
+	variants: {
+		...cvaVariants,
+	},
+	defaultVariants: {
+		...(cvaDefaults as any),
+	},
+})
+
+export interface TextProps extends React.HTMLAttributes<HTMLParagraphElement>, VariantProps<typeof cvaText> {}
+
+export const Paragraph: React.FC<TextProps> = ({
+	className,
+	size,
+	defaultLineHeight,
+	defaultLetterSpacing,
+	...props
+}) => <p className={cvaText({ className, size, defaultLineHeight, defaultLetterSpacing })} {...props} />
+
+export const Text: React.FC<TextProps> = ({ className, size, defaultLineHeight, defaultLetterSpacing, ...props }) => (
+	<span className={cvaText({ className, size, defaultLineHeight, defaultLetterSpacing })} {...props} />
+)
