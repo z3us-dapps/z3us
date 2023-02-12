37c7a1c71c85ff5f190f49b830e89b5a0240af6e
Zac Godinez <zac.godinez@tutanota.com>
2023-02-12 15:56:13 +0800

    test: remove dry run

--- apps/extension/src/containers/playground/containers/app/index.tsx
+++ apps/extension/src/containers/playground/containers/app/index.tsx
@@ -21,7 +21,7 @@ const NotFound404 = () => (
 			<Text size="xlarge">xlarge</Text>
 			<Text size="xxlarge">xxlarge</Text>
 			<Text size="xxxlarge" color="strong">
-				Testing new text
+				Testing new text more bumps on the test
 			</Text>
 		</Box>
 	</Box>
