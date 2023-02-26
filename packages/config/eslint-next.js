7aad3fc0e69dc77f27b2e1abd810892077e46c99
Zac Godinez <zac.godinez@tutanota.com>
2023-02-27 05:44:53 +0700

    feat: added eslint sort

--- packages/config/eslint-next.js
+++ packages/config/eslint-next.js
@@ -4,7 +4,7 @@ module.exports = {
 		node: true,
 	},
 	extends: ['next', 'airbnb', 'airbnb-typescript', 'plugin:import/recommended', 'plugin:import/typescript', 'prettier'],
-	plugins: ['@typescript-eslint', 'import', 'require-explicit-generics'],
+	plugins: ['@typescript-eslint', 'import', 'require-explicit-generics', 'simple-import-sort'],
 	settings: {
 		next: {
 			rootDir: ['apps/*/', 'packages/*/'],
