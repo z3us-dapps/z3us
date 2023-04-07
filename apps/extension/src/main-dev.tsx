cf6c8e8dd02f758716885ae8777618f162dce226
Zac Godinez <zac.godinez@tutanota.com>
2023-04-07 12:12:25 +0700

    fix: for react 18 in dev entry file

--- apps/extension/src/main-dev.tsx
+++ apps/extension/src/main-dev.tsx
@@ -1,19 +1,18 @@
 import React from 'react'
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { I18nextProvider } from 'react-i18next'
 
 import Playground from './containers/playground'
 import i18n from './i18n/i18n'
 
-const rootElement = document.getElementById('root')
+const container = document.getElementById('root')
 
-render(
+const root = createRoot(container) // createRoot(container!) if you use TypeScript
+
+root.render(
 	<React.StrictMode>
 		<I18nextProvider i18n={i18n}>
 			<Playground />
 		</I18nextProvider>
 	</React.StrictMode>,
-	rootElement,
 )
-
-rootElement.style.display = 'block'
