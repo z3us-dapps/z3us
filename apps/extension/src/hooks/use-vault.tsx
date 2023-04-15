852a81d198ffb6d814340edf45fd10ecba7a79c6
Heath Snee <heath.snee@gmail.com>
2023-04-15 17:53:01 +1000

    Remove log

--- apps/extension/src/hooks/use-vault.tsx
+++ apps/extension/src/hooks/use-vault.tsx
@@ -48,7 +48,6 @@ export const useVault = (messanger: MessageService) => {
 			publicKey: newSigningKey.publicKey,
 			network: n.id,
 		})
-		console.log(index)
 		addPublicAddress(index, {
 			...getDefaultAddressEntry(index),
 			...publicAddresses[index],
