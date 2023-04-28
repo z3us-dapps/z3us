dbe340ae23f105a7234459ade7e0d77929e5e237
Zac Godinez <zac.godinez@tutanota.com>
2023-04-28 19:22:46 +1000

    fix: combine the main tsx files

--- apps/extension/src/main.tsx
+++ apps/extension/src/main.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { I18nextProvider } from 'react-i18next'
 
+import Playground from '@src/containers/playground'
 import { Provider } from '@src/popup/provider'
+
+import i18n from './i18n/i18n'
+
+const isDevlopmentMode = import.meta.env.MODE === 'development'
 
 const container: HTMLElement | null = document.getElementById('root')
 const containerElement: HTMLElement = container!
@@ -10,6 +16,6 @@ const root = createRoot(containerElement)
 
 root.render(
 	<React.StrictMode>
-		<Provider />
+		<I18nextProvider i18n={i18n}>{isDevlopmentMode ? <Playground /> : <Provider />}</I18nextProvider>
 	</React.StrictMode>,
 )
