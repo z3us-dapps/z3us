5e7ba31e122ee74d45991597a3da12f656041efb
Zac Godinez <zac.godinez@tutanota.com>
2023-06-16 17:59:56 +1000

    fix: remove million

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,5 +1,3 @@
-
-const million = require("million/compiler");
 const { withContentlayer } = require("next-contentlayer");
 const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
 
@@ -7,34 +5,33 @@ const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import("next").NextConfig} */
 const config = {
-  experimental: {
-    appDir: false,
-  },
-  reactStrictMode: true,
-  swcMinify: true,
+	experimental: {
+		appDir: false,
+	},
+	reactStrictMode: true,
+	swcMinify: true,
 
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-  // Rewrite everything else to use `pages/index`
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/:path*',
-  //       destination: '/',
-  //     },
-  //   ];
-  // },
+	/**
+	 * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
+	 * out.
+	 *
+	 * @see https://github.com/vercel/next.js/issues/41980
+	 */
+	i18n: {
+		locales: ["en"],
+		defaultLocale: "en",
+	},
+	// Rewrite everything else to use `pages/index`
+	// async rewrites() {
+	//   return [
+	//     {
+	//       source: '/:path*',
+	//       destination: '/',
+	//     },
+	//   ];
+	// },
 };
-
 
 const nextConfig = withVanillaExtract(config);
 
-module.exports = withContentlayer(million.next(nextConfig));
+module.exports = withContentlayer(nextConfig);
