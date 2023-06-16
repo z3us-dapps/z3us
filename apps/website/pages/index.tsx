d925a788ce2ecde868c1a6977fd669f7d597c3ae
Zac Godinez <zac.godinez@tutanota.com>
2023-06-16 18:46:23 +1000

    fix: testing website

--- apps/website/pages/index.tsx
+++ apps/website/pages/index.tsx
@@ -18,6 +18,12 @@ import NextLink from "next/link";
 import React, { useState, useEffect } from "react";
 import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 
+// import { Box } from "ui/src/components-v2/box";
+// import {
+// 	darkThemeClass,
+// 	lightThemeClass,
+// } from "ui/src/components-v2/system/theme.css";
+
 declare global {
 	namespace JSX {
 		interface IntrinsicElements {
@@ -59,84 +65,90 @@ export default function App() {
 					content="https://example.com/images/cool-page.jpg"
 				/>
 			</Head>
-			<main className={styles.page}>
-				<Router>
-					<div>
-						<RdtProvider
-							value={RadixDappToolkit(
-								{
-									dAppName: "zgod",
-									dAppDefinitionAddress:
-										"account_tdx_c_1pxdceeqqfh9m4mt45cc0tqntyc5n87y4ze02p002yweq2y94zg",
-								},
-								(requestData) => {
-									requestData({
-										accounts: { quantifier: "atLeast", quantity: 1 },
-									});
-								},
-								{
-									networkId: 12,
-								}
-							)}
-						>
-							<div>
-								<div className={styles.radixButtonWrapper}>
-									<radix-connect-button className="test-class-rdx-btn"></radix-connect-button>
-								</div>
-							</div>
-							<ul>
-								<li>
-									<Link to="/">Home</Link>
-								</li>
-								<li>
-									<Link to="/about">About</Link>
-								</li>
-								<li>
-									<Link to="/topics">Topics</Link>
-								</li>
-								<li>
-									<NextLink href="/settings">Settings (SSR)</NextLink>
-								</li>
-								<li>
-									<NextLink href="/terms">Terms (SSR)</NextLink>
-								</li>
-							</ul>
-							<div>
-								<LogoTest />
-							</div>
 
-							<div
-								style={{
-									paddingTop: "30px",
-									paddingBottom: "30px",
-									display: "flex",
-									flexDirection: "column",
-									gap: "15px",
-								}}
+			{/* <main className={darkThemeClass}> */}
+			<main>
+				<div className={styles.page}>
+					<Router>
+						<div>
+							<RdtProvider
+								value={RadixDappToolkit(
+									{
+										dAppName: "zgod",
+										dAppDefinitionAddress:
+											"account_tdx_c_1pxdceeqqfh9m4mt45cc0tqntyc5n87y4ze02p002yweq2y94zg",
+									},
+									(requestData) => {
+										requestData({
+											accounts: { quantifier: "atLeast", quantity: 1 },
+										});
+									},
+									{
+										networkId: 12,
+									}
+								)}
 							>
-								<h2 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-									Theme
-								</h2>
-								<Button onClick={() => setTheme("light")}>Theme light</Button>
-								<Button onClick={() => setTheme("dark")}>Theme dark</Button>
-								<Button onClick={() => setTheme("system")}>Theme system</Button>
-							</div>
+								<div>
+									<div className={styles.radixButtonWrapper}>
+										<radix-connect-button className="test-class-rdx-btn"></radix-connect-button>
+									</div>
+								</div>
+								<ul>
+									<li>
+										<Link to="/">Home</Link>
+									</li>
+									<li>
+										<Link to="/about">About</Link>
+									</li>
+									<li>
+										<Link to="/topics">Topics</Link>
+									</li>
+									<li>
+										<NextLink href="/settings">Settings (SSR)</NextLink>
+									</li>
+									<li>
+										<NextLink href="/terms">Terms (SSR)</NextLink>
+									</li>
+								</ul>
+								<div>
+									<LogoTest />
+								</div>
 
-							<div>
-								<p>
-									<pre>hello</pre>
-									<div>hello</div>
-								</p>
-							</div>
+								<div
+									style={{
+										paddingTop: "30px",
+										paddingBottom: "30px",
+										display: "flex",
+										flexDirection: "column",
+										gap: "15px",
+									}}
+								>
+									<h2 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+										Theme
+									</h2>
+									<Button onClick={() => setTheme("light")}>Theme light</Button>
+									<Button onClick={() => setTheme("dark")}>Theme dark</Button>
+									<Button onClick={() => setTheme("system")}>
+										Theme system
+									</Button>
+								</div>
 
-							<Routes>
-								<Route path="/about" element={<h1>About</h1>} />
-								<Route path="/topics" element={<h1>Topics</h1>} />
-								<Route path="/" element={<h1>Home</h1>} />
-							</Routes>
-						</RdtProvider>
-					</div>
-				</Router>
+								<div>
+									<p>
+										<pre>hello</pre>
+										<div>hello</div>
+									</p>
+								</div>
+
+								<Routes>
+									<Route path="/about" element={<h1>About</h1>} />
+									<Route path="/topics" element={<h1>Topics</h1>} />
+									<Route path="/" element={<h1>Home</h1>} />
+								</Routes>
+							</RdtProvider>
+						</div>
+					</Router>
+				</div>
 			</main>
 		</>
 	);
