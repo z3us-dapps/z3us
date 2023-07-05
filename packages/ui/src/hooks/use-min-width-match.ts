491aba2348f57180859d2eb784fdf6803012a1d9
Heath Snee <heath.snee@gmail.com>
2023-07-05 18:20:58 +1000

    Add use min width match hook

---
+++ packages/ui/src/hooks/use-min-width-match.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+
+export const useMinWidthMatch = (minWidth: string): boolean => {
+	const [isMatch, setIsMatch] = useState<boolean>(true)
+
+	useEffect(() => {
+		const mql = window.matchMedia(`(min-width: ${minWidth})`)
+		const listener = (event: MediaQueryListEvent) => setIsMatch(event.matches)
+
+		mql.addEventListener('change', listener)
+		return () => {
+			mql.removeEventListener('change', listener)
+		}
+	}, [])
+
+	return isMatch
+}
