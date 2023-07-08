9604e710914d2c8b5512dfec717b33591db302a6
Heath Snee <heath.snee@gmail.com>
2023-07-08 11:28:29 +1000

    Derive legacy keys

--- apps/extension/src/crypto/key_pair.ts
+++ apps/extension/src/crypto/key_pair.ts
@@ -6,15 +6,25 @@ import hdkey from 'hdkey'
 import { entropyToMnemonic } from '@src/crypto/mnemonic'
 import { type Data, DataType } from '@src/types/vault'
 
-export function legacyEntropyToPrivateKey(entropy: string): PrivateKey.IPrivateKey {
+export function legacyDerivePrivateKeyFromIndex(
+	privateKey: PrivateKey.Secp256k1,
+	index: number = 0,
+): PrivateKey.Secp256k1 {
+	const root = hdkey.fromMasterSeed(privateKey.toString())
+	// `m/${purpose}'/${coin_type}'/${account}'/${change}/${address_index}`
+	// defaults to: `m/44'/1022'/0'/0/0`
+	const key = root.derive(`m/44'/1022'/0'/0/${index}`)
+	return new PrivateKey.Secp256k1(key.toString('hex'))
+}
+
+export function legacyEntropyToPrivateKey(entropy: string): PrivateKey.Secp256k1 {
 	const mnemonic = entropyToMnemonic(entropy)
 	const seed = bip39.mnemonicToSeedSync(mnemonic).toString('hex')
 	const root = hdkey.fromMasterSeed(Buffer.from(seed, 'hex'))
-	// const key = root.derive("m/44'/1022'/0'/0/0")
 	return new PrivateKey.Secp256k1(root.privateKey.toString('hex'))
 }
 
-export function fromExtendedPrivateKey(xpriv: string): PrivateKey.IPrivateKey {
+export function fromExtendedPrivateKey(xpriv: string): PrivateKey.Secp256k1 {
 	const root = hdkey.fromJSON({ xpriv, xpub: 'not used' })
 	return new PrivateKey.Secp256k1(root.privateKey.toString('hex'))
 }
