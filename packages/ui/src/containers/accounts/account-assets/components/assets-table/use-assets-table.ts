3a95c0c1f9ccab197d020a0a478af0c51e3f9977
Zac Godinez <zac.godinez@tutanota.com>
2023-07-23 16:19:02 +1000

    refactor: table updates

--- packages/ui/src/containers/accounts/account-assets/components/assets-table/use-assets-table.ts
+++ packages/ui/src/containers/accounts/account-assets/components/assets-table/use-assets-table.ts
@@ -1,5 +1,5 @@
-import React, { useEffect, useMemo, useState } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useEffect, useMemo, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import { useAccountParams } from 'ui/src/hooks/use-account-params'
 
@@ -7,17 +7,15 @@ import { AssetNameCell } from './asset-name-cell'
 import { AssetStatisticCell } from './asset-statistic-cell'
 import * as styles from './assets-table.css'
 
-// eslint-disable-next-line arrow-body-style
-export const generateRandomString = () => Math.random().toString(36).substring(7)
+const generateRandomString = () => Math.random().toString(36).substring(7)
 
-// fix types
 type TAssetsTable = {
 	items: any
 	columns: any
-	loading: any
-	loadMore: any
-	onRowSelected: any
-	onEndReached: any
+	loading: boolean
+	loadMore: boolean
+	onRowSelected: (row: any) => void
+	onEndReached: () => void
 }
 
 export const useAssetsTable = (): TAssetsTable => {
@@ -28,8 +26,8 @@ export const useAssetsTable = (): TAssetsTable => {
 	const [loading, setLoading] = useState<boolean>(true)
 	const [loadMore, setLoadMore] = useState<boolean>(false)
 
-	const columns = useMemo(
-		() => [
+	const columns = useMemo(() => {
+		let cols = [
 			{
 				Header: 'Token',
 				accessor: 'token',
@@ -58,52 +56,78 @@ export const useAssetsTable = (): TAssetsTable => {
 				Cell: AssetStatisticCell,
 				className: styles.mobileHideTableCellWrapper,
 			},
-		],
-		[account, assetType, asset],
-	)
+		]
+
+		if (assetType) {
+			cols = [
+				{
+					Header: 'Token',
+					accessor: 'token',
+					width: '50%',
+					Cell: AssetNameCell,
+				},
+			]
+		}
+
+		if (asset) {
+			cols = [
+				{
+					Header: 'Token',
+					accessor: 'token',
+					width: '50%',
+					Cell: AssetNameCell,
+				},
+				{
+					Header: 'Portfolio',
+					accessor: 'portfolio',
+					width: 'auto',
+					Cell: AssetStatisticCell,
+					className: styles.mobileHideTableCellWrapper,
+					test: 'test',
+				},
+			]
+		}
+
+		return cols
+	}, [account, assetType, asset])
 
 	useEffect(() => {
 		setLoading(true)
 		setTimeout(() => {
-			setItems(
-				Array.from({ length: 50 }).map((_, i, a) => {
-					const randomStr = generateRandomString()
-					return {
-						id: randomStr,
-						token: `${account} - ${randomStr}`,
-						portfolio: '65%',
-						price: '$1.83',
-						balance: `99`,
-					}
-				}),
-			)
-
+			const newItems = Array.from({ length: 50 }).map((_, i, a) => {
+				const randomStr = generateRandomString()
+				return {
+					id: randomStr,
+					token: `${account} - ${randomStr}`,
+					portfolio: '65%',
+					price: '$1.83',
+					balance: '99',
+				}
+			})
+			setItems(newItems)
 			setLoading(false)
 		}, 1000)
-	}, [account])
+	}, [account, assetType, asset])
 
 	const handleRowSelected = (row: any) => {
 		const { original } = row
-
 		navigate(`/accounts/${account}/${original.id}`)
 	}
 
 	const handleEndReached = () => {
 		setLoadMore(true)
 		setTimeout(() => {
-			setItems(prev => [
-				...prev,
-				...Array.from({ length: 50 }).map((_, i, a) => {
-					const randomStr = generateRandomString()
-					return {
-						id: randomStr,
-						token: `${account} - ${randomStr}`,
-						portfolio: '65%',
-						price: '$1.83',
-						balance: `99`,
-					}
-				}),
-			])
+			const newItems = Array.from({ length: 50 }).map((_, i, a) => {
+				const randomStr = generateRandomString()
+				return {
+					id: randomStr,
+					token: `${account} - ${randomStr}`,
+					portfolio: '65%',
+					price: '$1.83',
+					balance: '99',
+				}
+			})
+			setItems(prev => [...prev, ...newItems])
 			setLoadMore(false)
 		}, 2000)
 	}
