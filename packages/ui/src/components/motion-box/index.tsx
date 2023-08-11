52f57836dca76349674432f11e3b27a03c1489f1
Zac Godinez <zac.godinez@tutanota.com>
2023-08-12 08:01:07 +1000

    style: motion box styles

--- packages/ui/src/components/motion-box/index.tsx
+++ packages/ui/src/components/motion-box/index.tsx
@@ -2,24 +2,16 @@ import { motion } from 'framer-motion'
 import type { PropsWithChildren } from 'react'
 import React from 'react'
 
+import * as styles from './styles.css'
+
 const MotionBox: React.FC<PropsWithChildren> = ({ children }) => (
 	<motion.div
-		style={{
-			zIndex: 10,
-			position: 'absolute',
-			height: '50vh',
-			width: '50vw',
-			backgroundColor: 'skyblue',
-		}}
-		initial={{
-			scale: 0,
-		}}
-		animate={{ scale: 1, left: 0, top: '200px' }}
-		exit={{
-			scale: 0,
-		}}
+		className={styles.animatedPageWrapper}
+		initial={{ scale: 0.95, opacity: 0 }}
+		animate={{ scale: 1, opacity: 1 }}
+		exit={{ scale: 0.95, opacity: 0 }}
 		transition={{
-			duration: 3,
+			duration: 0.3,
 			ease: 'easeOut',
 		}}
 	>
