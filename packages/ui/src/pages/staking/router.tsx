a23af4e92ebdd9ac3c997a6cf952e39466565234
Heath Snee <heath.snee@gmail.com>
2023-08-11 17:41:24 +1000

    Add staking layout

--- packages/ui/src/pages/staking/router.tsx
+++ packages/ui/src/pages/staking/router.tsx
@@ -1,10 +1,18 @@
 import { lazy } from 'react'
 
+import Layout from './components/layout'
+
 const Home = lazy(() => import('./home'))
 
 const route = {
 	path: 'staking',
-	element: <Home />,
+	element: <Layout />,
+	children: [
+		{
+			index: true,
+			element: <Home />,
+		},
+	],
 }
 
 export default route
