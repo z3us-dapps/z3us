d64be1c9dfa4038b43e43401f3b1a31ca122e901
Zac Godinez <zac.godinez@tutanota.com>
2023-08-15 08:52:50 +1000

    style: settings mobile menu

--- packages/ui/src/pages/settings/components/navigation/index.tsx
+++ packages/ui/src/pages/settings/components/navigation/index.tsx
@@ -2,35 +2,40 @@ import { LayoutGroup } from 'framer-motion'
 import { t } from 'i18next'
 import { PillNavigation } from 'packages/ui/src/components/pill-navigation'
 import React from 'react'
+import { Navigate } from 'react-router-dom'
 
 import { Box } from 'ui/src/components/box'
 import { AddressBookIcon, CoinsIcon, Settings2Icon } from 'ui/src/components/icons'
+import { MobileStackedNavigation } from 'ui/src/components/layout/mobile-stacked-navigation'
 import { NavLink } from 'ui/src/components/router-link'
+import { useIsMobileWidth } from 'ui/src/hooks/use-is-mobile'
 
-const Navigation: React.FC = () => (
+const SETTINGS_MENU = [
+	{
+		title: t('settings.navigation.generalTitle'),
+		subTitle: t('settings.navigation.generalSubTitle'),
+		href: '/settings/general',
+		icon: <Settings2Icon />,
+	},
+	{
+		title: t('settings.navigation.accountsTitle'),
+		subTitle: t('settings.navigation.accountsSubTitle'),
+		href: '/settings/accounts',
+		icon: <CoinsIcon />,
+	},
+	{
+		title: t('settings.navigation.accountsAddressBookTitle'),
+		subTitle: t('settings.navigation.accountsAddressBookSubTitle'),
+		href: '/settings/address-book',
+		icon: <AddressBookIcon />,
+	},
+]
+
+export const DesktopNavigation: React.FC = () => (
 	<nav>
 		<Box component="ul">
 			<LayoutGroup id="settings-menu">
-				{[
-					{
-						title: t('settings.navigation.generalTitle'),
-						subTitle: t('settings.navigation.generalSubTitle'),
-						href: '/settings/general',
-						icon: <Settings2Icon />,
-					},
-					{
-						title: t('settings.navigation.accountsTitle'),
-						subTitle: t('settings.navigation.accountsSubTitle'),
-						href: '/settings/accounts',
-						icon: <CoinsIcon />,
-					},
-					{
-						title: t('settings.navigation.accountsAddressBookTitle'),
-						subTitle: t('settings.navigation.accountsAddressBookSubTitle'),
-						href: '/settings/address-book',
-						icon: <AddressBookIcon />,
-					},
-				].map(({ title, href }) => (
+				{SETTINGS_MENU.map(({ title, href }) => (
 					<Box key={href} component="li">
 						<NavLink to={href} underline="never">
 							{({ isActive }) => <PillNavigation text={title} matchActiveFn={() => isActive} />}
@@ -42,4 +47,16 @@ const Navigation: React.FC = () => (
 	</nav>
 )
 
-export default Navigation
+export const MobileNavigation: React.FC = () => {
+	const isMobile = useIsMobileWidth()
+
+	if (!isMobile) {
+		return <Navigate to="/settings/general" />
+	}
+
+	return (
+		<Box component="ul" paddingX="small">
+			<MobileStackedNavigation menu={SETTINGS_MENU} />
+		</Box>
+	)
+}
