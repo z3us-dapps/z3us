c749c57beef581fc6c52a87a39c101e291d266af
Heath Snee <heath.snee@gmail.com>
2023-08-18 15:52:06 +1000

    Add submit button

--- packages/ui/src/components/form/index.tsx
+++ packages/ui/src/components/form/index.tsx
@@ -1,18 +1,44 @@
-import type { PropsWithChildren } from 'react'
+import type { FormEvent, PropsWithChildren } from 'react'
 import React from 'react'
+
+import { Box } from 'ui/src/components/box'
+import { Button } from 'ui/src/components/button'
+import Translation from 'ui/src/components/translation'
+
+import { LoadingBarsIcon } from '../icons'
 
 interface IProps {
 	onSubmit: (event: React.FormEvent) => void
 }
 
 export const Form: React.FC<PropsWithChildren<IProps>> = ({ children, onSubmit, ...rest }) => {
-	const handleSubmit = (e: React.FormEvent) => {
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
 		onSubmit(e)
 	}
+
+	const isLoading = false // @TODO
 
 	return (
 		<form {...rest} onSubmit={handleSubmit}>
 			{children}
+
+			<Box display="flex" paddingTop="xlarge" width="full">
+				<Button
+					styleVariant="primary"
+					sizeVariant="xlarge"
+					type="submit"
+					disabled={isLoading}
+					rightIcon={
+						isLoading ? (
+							<Box marginLeft="small">
+								<LoadingBarsIcon />
+							</Box>
+						) : null
+					}
+				>
+					<Translation capitalizeFirstLetter text="global.submit" />
+				</Button>
+			</Box>
 		</form>
 	)
 }
--- packages/ui/src/pages/transfer/demo/index.tsx
+++ packages/ui/src/pages/transfer/demo/index.tsx
@@ -17,7 +17,7 @@ export const Demo: React.FC = () => {
 
 	const handleSubmit = (e: React.FormEvent) => {
 		e.preventDefault()
-		console.log(e.currentTarget)
+		console.log((e.currentTarget as any).elements)
 	}
 
 	return (
