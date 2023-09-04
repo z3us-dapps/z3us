69f02109c8ce130ba8c71f6e6f799c0e76f6d392
Zac Godinez <zac.godinez@tutanota.com>
2023-09-05 09:57:04 +1000

    refactor: added list component for home and assets

--- packages/ui/src/pages/accounts/home/index.tsx
+++ packages/ui/src/pages/accounts/home/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import useMeasure from 'react-use-measure'
 
 import { Box } from 'ui/src/components/box'
+import Loader from 'ui/src/components/loader'
 import {
 	ScrollAreaRoot,
 	ScrollAreaScrollbar,
@@ -10,16 +12,33 @@ import {
 } from 'ui/src/components/scroll-area-radix'
 import Translation from 'ui/src/components/translation'
 import { Text } from 'ui/src/components/typography'
+import { useSelectedAccounts } from 'ui/src/hooks/dapp/use-accounts'
+import { useBalances } from 'ui/src/hooks/dapp/use-balances'
 import { useWalletAccounts } from 'ui/src/hooks/use-wallet-account'
 
+import { AssetsList } from '../components/assets-list'
 import { HomeScrollShadow } from '../components/home-scroll-shadow'
 import { AccountHomeCard } from './components/account-home-card'
-import { AccountsList } from './components/accounts-list'
 import * as styles from './styles.css'
 
 const Home: React.FC = () => {
 	const accounts = useWalletAccounts()
+
+	const { accountId = '-' } = useParams()
 	const [wrapperRef, { width: horizontalScrollWidth }] = useMeasure()
+
+	const selectedAccounts = useSelectedAccounts()
+	const {
+		isLoading,
+		fungibleBalances,
+		fungibleChange,
+		fungibleValue,
+		nonFungibleBalances,
+		nonFungibleChange,
+		nonFungibleValue,
+	} = useBalances(...selectedAccounts)
+
+	if (isLoading) return <Loader />
 
 	return (
 		<Box ref={wrapperRef} className={styles.assetsHomeWrapper}>
@@ -45,7 +64,15 @@ const Home: React.FC = () => {
 					<Translation text="accounts.home.allAssets" />
 				</Text>
 			</Box>
-			<AccountsList />
+			<AssetsList
+				accountId={accountId}
+				fungibleBalances={fungibleBalances}
+				fungibleValue={fungibleValue}
+				fungibleChange={fungibleChange}
+				nonFungibleBalances={nonFungibleBalances}
+				nonFungibleValue={nonFungibleValue}
+				nonFungibleChange={nonFungibleChange}
+			/>
 		</Box>
 	)
 }
