d1f0a44659c7766eb57180cb27ef04c1b89c09ee
Heath Snee <heath.snee@gmail.com>
2023-09-11 18:21:25 +1000

    Fix keystore migration

--- apps/extension/src/browser/background/install.ts
+++ apps/extension/src/browser/background/install.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { getNoneSharedStore } from 'packages/ui/src/services/state'
 import { sharedStore } from 'packages/ui/src/store'
 import { type Keystore, KeystoreType, type Keystore as NewKeystore } from 'packages/ui/src/store/types'
@@ -6,65 +7,61 @@ import browser from 'webextension-polyfill'
 
 const migrateOlympiaAddresses = async () => {
 	const oldSharedStore = await browser.storage.local.get(['z3us-store-shared'])
-	const oldSharedState = JSON.parse(oldSharedStore['z3us-store-shared'] || {}).state
-	if (oldSharedState) {
-		const currentState = sharedStore.getState()
+	if (!oldSharedStore['z3us-store-shared']) return
 
-		const newKeystores = await Promise.all(
-			oldSharedState.keystores.map(async (keystore: Keystore) => {
-				try {
-					if (keystore.type !== KeystoreType.LOCAL) return null
+	const oldSharedState = JSON.parse(oldSharedStore['z3us-store-shared']).state
+	if (oldSharedState) return
 
-					const oldNoneSharedStore = await browser.storage.local.get(`z3us-store-${keystore.id}`)
-					const oldNoneSharedState = JSON.parse(oldNoneSharedStore[`z3us-store-${keystore.id}`] || {}).state
-					if (!oldNoneSharedState) return null
+	const newKeystores = await Promise.all(
+		oldSharedState.keystores.map(async (keystore: Keystore) => {
+			try {
+				if (keystore.type !== KeystoreType.LOCAL) return null
 
-					const noneSharedStore = await getNoneSharedStore(keystore.id)
-					const currentKeystoreState = noneSharedStore.getState()
-					currentKeystoreState.olympiaAddresses = oldNoneSharedState?.publicAddresses || undefined
-					noneSharedStore.setState(currentKeystoreState)
-					await noneSharedStore.persist.rehydrate()
+				const oldNoneSharedStore = await browser.storage.local.get(`z3us-store-${keystore.id}`)
+				if (!oldNoneSharedStore[`z3us-store-${keystore.id}`]) return null
 
-					return {
-						...keystore,
-					} as NewKeystore
-				} catch (error) {
-					// eslint-disable-next-line no-console
-					console.error(`migrateOlympiaAddresses: ${JSON.stringify(keystore)}: ${error}`)
-				}
-				return null
-			}),
-		)
+				const oldNoneSharedState = JSON.parse(oldNoneSharedStore[`z3us-store-${keystore.id}`] || {}).state
+				if (!oldNoneSharedState) return null
 
-		currentState.keystores = [...currentState.keystores, ...newKeystores.filter(keystore => !!keystore)]
-		sharedStore.setState(currentState)
-		await sharedStore.persist.rehydrate()
+				const noneSharedStore = await getNoneSharedStore(keystore.id)
+				const currentKeystoreState = noneSharedStore.getState()
+				currentKeystoreState.olympiaAddresses = oldNoneSharedState?.publicAddresses || {}
+				noneSharedStore.setState(currentKeystoreState)
 
-		try {
-			await browser.storage.local.remove('z3us-store-shared')
-		} catch (error) {
-			// eslint-disable-next-line no-console
-			console.error(`failed to remove old shared store: ${error}`)
-		}
-		await Promise.all(
-			oldSharedState.keystores.map(async keystore => {
-				try {
-					await browser.storage.local.remove(`z3us-store-${keystore.id}`)
-				} catch (error) {
-					// eslint-disable-next-line no-console
-					console.error(`failed to remove none shared store for: ${JSON.stringify(keystore)}: ${error}`)
-				}
-			}),
-		)
+				return {
+					...keystore,
+				} as NewKeystore
+			} catch (error) {
+				console.error(`migrateOlympiaAddresses: ${JSON.stringify(keystore)}: ${error}`)
+			}
+			return null
+		}),
+	)
+
+	const currentState = sharedStore.getState()
+	currentState.keystores = [...currentState.keystores, ...newKeystores.filter(keystore => !!keystore)]
+	sharedStore.setState(currentState)
+
+	await Promise.all(
+		oldSharedState.keystores.map(async keystore => {
+			try {
+				// await browser.storage.local.remove(`z3us-store-${keystore.id}`)
+			} catch (error) {
+				console.error(`failed to remove none shared store for: ${JSON.stringify(keystore)}: ${error}`)
+			}
+		}),
+	)
+
+	try {
+		// await browser.storage.local.remove('z3us-store-shared')
+	} catch (error) {
+		console.error(`failed to remove old shared store: ${error}`)
 	}
 }
 
 export const handleInstall = async (details: Runtime.OnInstalledDetailsType) => {
-	if (details.reason === 'update' && details.previousVersion === '1.0.7') {
-		const [major] = details.previousVersion.replace(/[^\d.-]+/g, '').split(/[.-]/)
-		if (parseInt(major, 10) <= 2) {
-			await migrateOlympiaAddresses()
-		}
+	if (details.reason === 'update') {
+		await migrateOlympiaAddresses()
 	}
 
 	browser.runtime.setUninstallURL('https://github.com/z3us-dapps/z3us/discussions/150')
