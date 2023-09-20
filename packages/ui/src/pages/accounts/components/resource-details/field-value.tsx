41b6ef8c5169220cdd84be32793320018ef0b145
Heath Snee <heath.snee@gmail.com>
2023-09-20 20:52:37 +1000

    Remove error log

--- packages/ui/src/pages/accounts/components/resource-details/field-value.tsx
+++ packages/ui/src/pages/accounts/components/resource-details/field-value.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 
 export const getDataValue = (field?: any) => {
-
-	console.error(field, field?.fields?.map(getDataValue))
 	return field?.value ? field?.value || '' : field?.fields?.map(getDataValue).join(', ') || ''
 }
 
