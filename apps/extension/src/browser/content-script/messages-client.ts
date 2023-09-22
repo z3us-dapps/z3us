45954169df18433d7a125c2a02d8a2179e03b50e
Heath Snee <heath.snee@gmail.com>
2023-09-22 13:36:02 +1000

    Lint

--- apps/extension/src/browser/content-script/messages-client.ts
+++ apps/extension/src/browser/content-script/messages-client.ts
@@ -6,8 +6,8 @@ import { PORT_NAME } from '@src/browser/messages/constants'
 import type { Message } from '@src/browser/messages/types'
 import { MessageAction, MessageSource } from '@src/browser/messages/types'
 
-import { addMetadata } from '../metadata/add'
 import { newMessage } from '../messages/message'
+import { addMetadata } from '../metadata/add'
 import { chromeDAppClient, radixMessageHandler, sendRadixMessage, sendRadixMessageToDapp } from './radix'
 import { isHandledByRadix } from './radix-connector'
 
