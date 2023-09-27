6553192ce65ce5f2bb115d61c1b0128d888ab63c
Zac Godinez <zac.godinez@tutanota.com>
2023-09-28 08:43:07 +1000

    refactor: clean up the account total value

--- packages/ui/src/pages/accounts/components/layout/components/total-value/index.tsx
+++ packages/ui/src/pages/accounts/components/layout/components/total-value/index.tsx
@@ -41,8 +41,6 @@ export const AccountTotalValue: React.FC<IProps> = props => {
 		poolUnitsChange,
 	} = useBalances(...selectedAccounts)
 
-	const [hidden, setHidden] = useState<boolean>(false)
-
 	const value = useMemo(() => {
 		if (resourceType === 'nfts') return nonFungibleValue
 		if (resourceType === 'tokens') return fungibleValue
@@ -59,38 +57,18 @@ export const AccountTotalValue: React.FC<IProps> = props => {
 		return totalChange
 	}, [resourceType, totalValue, fungibleValue, nonFungibleValue])
 
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const handleToggleHidden = () => {
-		setHidden(!hidden)
-	}
-
 	return (
 		<Box className={clsx(styles.assetsHeaderWrapper, className)}>
-			<Box display="flex" width="full">
-				<Box flexGrow={1}>
-					<Box display="flex" alignItems="center" gap="small">
-						<Box flexGrow={1} display="flex" flexDirection="column" gap="xxsmall">
-							<Box display="flex" alignItems="center" gap="medium">
-								<TextScramble scramble={hidden}>
-									<Text weight="medium" size="xxxlarge" color="strong" truncate blur={hidden}>
-										{isLoading ? 'Loading...' : intl.formatNumber(value.toNumber(), { style: 'currency', currency })}
-									</Text>
-								</TextScramble>
-								{/* TODO: */}
-								{/* <ToolTip message={hidden ? 'accounts.home.accountShowBalance' : 'accounts.home.accountHideBalance'}>
-									<Button onClick={handleToggleHidden} styleVariant="ghost" sizeVariant="small" iconOnly>
-										{hidden ? <EyeIcon /> : <EyeOffIcon />}
-									</Button>
-								</ToolTip> */}
-							</Box>
-							<TextScramble scramble={hidden}>
-								<Text size="xxsmall" weight="medium" color={change && change.gt(0) ? 'green' : 'red'} truncate>
-									{intl.formatNumber(change.toNumber(), { style: 'percent', maximumFractionDigits: 2 })}
-								</Text>
-							</TextScramble>
-						</Box>
-					</Box>
+			<Box flexGrow={1} display="flex" flexDirection="column" gap="xxsmall">
+				<Box display="flex" alignItems="center" gap="medium">
+					<Text weight="medium" size="xxxlarge" color="strong" truncate>
+						{/* TODO: handle loading state */}
+						{isLoading ? 'Loading...' : intl.formatNumber(value.toNumber(), { style: 'currency', currency })}
+					</Text>
 				</Box>
+				<Text size="xxsmall" weight="medium" color={change && change.gt(0) ? 'green' : 'red'} truncate>
+					{intl.formatNumber(change.toNumber(), { style: 'percent', maximumFractionDigits: 2 })}
+				</Text>
 			</Box>
 		</Box>
 	)
