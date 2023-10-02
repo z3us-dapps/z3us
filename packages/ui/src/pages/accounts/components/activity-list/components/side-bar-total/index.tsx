3d0251bdecf1ce833e1753ff9297c6baa2c8fb0f
Zac Godinez <zac.godinez@tutanota.com>
2023-10-02 13:09:23 +1100

    refactor: pr feedback

--- packages/ui/src/pages/accounts/components/activity-list/components/side-bar-total/index.tsx
+++ packages/ui/src/pages/accounts/components/activity-list/components/side-bar-total/index.tsx
@@ -22,6 +22,10 @@ export const SideBarTotal: React.FC = () => {
 
 	const { isLoading, formattedValue, formattedChange, changeStatus } = useTotalBalance()
 
+	if (isLoading) {
+		return null
+	}
+
 	return (
 		<Box className={clsx(styles.mobileHomeBalanceWrapper, !isAllAccounts && styles.mobileHiddenWrapper)}>
 			<Text color="strong" size="xlarge">
@@ -29,7 +33,7 @@ export const SideBarTotal: React.FC = () => {
 			</Text>
 			<Box display="flex" alignItems="center" gap="small">
 				<Text weight="medium" size="xxxlarge" color="strong" truncate>
-					{isLoading ? 'Loading ...' : formattedValue}
+					{formattedValue}
 				</Text>
 				<RedGreenText size="xxsmall" weight="medium" truncate changeStatus={changeStatus}>
 					{formattedChange}
--- packages/ui/src/pages/accounts/components/layout/components/total-value/index.tsx
+++ packages/ui/src/pages/accounts/components/layout/components/total-value/index.tsx
@@ -10,12 +10,16 @@ import * as styles from './styles.css'
 export const AccountTotalValue: React.FC = () => {
 	const { isLoading, formattedValue, formattedChange, changeStatus } = useTotalBalance()
 
+	if (isLoading) {
+		return null
+	}
+
 	return (
 		<Box className={styles.assetsHeaderWrapper}>
 			<Box flexGrow={1} display="flex" flexDirection="column" gap="xxsmall">
 				<Box display="flex" alignItems="center" gap="medium">
 					<Text weight="medium" size="xxxlarge" color="strong" truncate>
-						{isLoading ? 'Loading ...' : formattedValue}
+						{formattedValue}
 					</Text>
 				</Box>
 				<RedGreenText size="xxsmall" weight="medium" truncate changeStatus={changeStatus}>
