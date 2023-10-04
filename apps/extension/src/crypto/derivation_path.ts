4443b9fd5440c82852c380b912756ce939f3cfe1
Heath Snee <heath.snee@gmail.com>
2023-10-04 22:23:58 +1100

    Add comments

--- apps/extension/src/crypto/derivation_path.ts
+++ apps/extension/src/crypto/derivation_path.ts
@@ -5,3 +5,27 @@ export function buildPersonaDerivationPath(idx: number) {
 export function buildAccountDerivationPath(idx: number) {
 	return `m/44H/1022H/14H/525H/1460H/${idx}H`
 }
+
+
+// ledger
+
+// Ed222519Challenge
+// m/44H/1022H/12H/525H/1460H/0H
+
+// Ed25519Transaction
+// m/44H/1022H/10H/525H/1460H/0H
+
+
+// Secp256k1Challenge
+// m/44H/1022H/10H/525H/1238H
+
+// Secp256k1Transaction
+// m/44H/1022H/10H/525H/1238H
+
+
+
+// derivePublicKeys - curve25519
+// m/44H/1022H/10H/525H/1460H/0H
+
+// deriveAndDisplayAddress - curve25519
+// m/44H/1022H/10H/525H/1460H/0H
