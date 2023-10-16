714568b17064dbd56ec2a516776e028b14fac27d
Zac Godinez <zac.godinez@tutanota.com>
2023-10-17 09:18:06 +1100

    fix: upgrade storybook

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,16 +1,17 @@
+import { dirname, join } from "path";
 const { mergeConfig } = require('vite')
 
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
 	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-interactions',
-		'@storybook/addon-viewport',
-		'storybook-dark-mode',
+		getAbsolutePath("@storybook/addon-links"),
+		getAbsolutePath("@storybook/addon-essentials"),
+		getAbsolutePath("@storybook/addon-interactions"),
+		getAbsolutePath("@storybook/addon-viewport"),
+		getAbsolutePath("storybook-dark-mode"),
 	],
 	framework: {
-		name: '@storybook/react-vite',
+		name: getAbsolutePath("@storybook/react-vite"),
 		options: {},
 	},
 	docs: {
@@ -21,4 +22,8 @@ module.exports = {
 			plugins: [require('@vanilla-extract/vite-plugin').vanillaExtractPlugin()],
 		})
 	},
+}
+
+function getAbsolutePath(value: string): any {
+    return dirname(require.resolve(join(value, "package.json")));
 }
