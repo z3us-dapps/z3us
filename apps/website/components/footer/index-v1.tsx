0d682b48beac4d1b76084957114f770f95b87711
Zac Godinez <zac.godinez@tutanota.com>
2023-10-20 16:47:37 +1100

    refactor: clean lints in website and ui

--- apps/website/components/footer/index-v1.tsx
+++ apps/website/components/footer/index-v1.tsx
@@ -1,49 +1,49 @@
-import { PageContainer } from 'components/page-container'
-import Link from 'next/link'
-import React from 'react'
+// import { PageContainer } from 'components/page-container'
+// import Link from 'next/link'
+// import React from 'react'
 
-import { Z3usLogo } from 'ui/src/components/z3us-logo'
+// import { Z3usLogo } from 'ui/src/components/z3us-logo'
 
-import { ThemeSelector } from './theme-selector'
+// import { ThemeSelector } from './theme-selector'
 
-interface IProps {
-	className?: string | undefined
-}
+// interface IProps {
+// 	className?: string | undefined
+// }
 
-const defaultProps = {
-	className: undefined,
-}
+// const defaultProps = {
+// 	className: undefined,
+// }
 
-export const Footer = ({ className }: IProps) => (
-	<div className={`footer ${className}`}>
-		<PageContainer>
-			<div className="block sm:flex w-100 items-center">
-				<div className="flex-1 flex items-center fill-white">
-					<Link href="/" passHref>
-						<a className="cursor-pointer inline-flex items-center justify-center hover:opacity-80 transition-opacity">
-							<Z3usLogo className="transition-colors" />
-						</a>
-					</Link>
-					<span className="text-xs pl-2">&copy; {new Date().getFullYear()} Z3US</span>
-				</div>
-				<ul className="text-sm flex items-center pt-2 sm:pt-0">
-					<li className="pr-3">
-						<Link href="/privacy" passHref>
-							<a className="hover:underline decoration-from-font underline-offset-4">Privacy</a>
-						</Link>
-					</li>
-					<li className="pr-2">
-						<Link href="/terms" passHref>
-							<a className="hover:underline decoration-from-font underline-offset-4">Terms</a>
-						</Link>
-					</li>
-					<li>
-						<ThemeSelector />
-					</li>
-				</ul>
-			</div>
-		</PageContainer>
-	</div>
-)
+// export const Footer = ({ className }: IProps) => (
+// 	<div className={`footer ${className}`}>
+// 		<PageContainer>
+// 			<div className="block sm:flex w-100 items-center">
+// 				<div className="flex-1 flex items-center fill-white">
+// 					<Link href="/" passHref>
+// 						<a className="cursor-pointer inline-flex items-center justify-center hover:opacity-80 transition-opacity">
+// 							<Z3usLogo className="transition-colors" />
+// 						</a>
+// 					</Link>
+// 					<span className="text-xs pl-2">&copy; {new Date().getFullYear()} Z3US</span>
+// 				</div>
+// 				<ul className="text-sm flex items-center pt-2 sm:pt-0">
+// 					<li className="pr-3">
+// 						<Link href="/privacy" passHref>
+// 							<a className="hover:underline decoration-from-font underline-offset-4">Privacy</a>
+// 						</Link>
+// 					</li>
+// 					<li className="pr-2">
+// 						<Link href="/terms" passHref>
+// 							<a className="hover:underline decoration-from-font underline-offset-4">Terms</a>
+// 						</Link>
+// 					</li>
+// 					<li>
+// 						<ThemeSelector />
+// 					</li>
+// 				</ul>
+// 			</div>
+// 		</PageContainer>
+// 	</div>
+// )
 
-Footer.defaultProps = defaultProps
+// Footer.defaultProps = defaultProps
