7189de7e633b016620940fd7e96f5f8be380bbe7
Zac Godinez <zac.godinez@tutanota.com>
2023-10-21 10:16:59 +1100

    style: lint fix

--- packages/ui/src/pages/settings/components/navigation/index.tsx
+++ packages/ui/src/pages/settings/components/navigation/index.tsx
@@ -4,8 +4,13 @@ import { PageMenu } from 'ui/src/components/navigation/page-menu'
 
 import { useMenuItems } from './use-menu-items'
 
-export const DesktopNavigation: React.FC = () => {
+interface IProps {
+	title: string | React.ReactElement
+}
+
+export const DesktopNavigation: React.FC<IProps> = props => {
+	const { title } = props
 	const menuItems = useMenuItems()
 
-	return <PageMenu id="settings-menu" menu={menuItems} />
+	return <PageMenu id="settings-menu" menu={menuItems} title={title} />
 }
