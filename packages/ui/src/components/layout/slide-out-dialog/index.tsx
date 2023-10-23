e21f54e4216b0ee5aa7800347564b95ad155504a
Zac Godinez <zac.godinez@tutanota.com>
2023-10-24 09:24:37 +1100

    fix: remove tooltip from close button in slide out dialog

--- packages/ui/src/components/layout/slide-out-dialog/index.tsx
+++ packages/ui/src/components/layout/slide-out-dialog/index.tsx
@@ -1,22 +1,14 @@
 import clsx from 'clsx'
 import React from 'react'
-import { defineMessages, useIntl } from 'react-intl'
+import { useIntl } from 'react-intl'
 
 import { Box } from 'ui/src/components/box'
-import { DialogContent, DialogOverlay, DialogPortal, DialogRoot } from 'ui/src/components/dialog'
+import { DialogClose, DialogContent, DialogOverlay, DialogPortal, DialogRoot } from 'ui/src/components/dialog'
 import { Close2Icon } from 'ui/src/components/icons'
 import { Button } from 'ui/src/components/router-button'
 import { ScrollAreaRadix as ScrollArea } from 'ui/src/components/scroll-area-radix'
-import { ToolTip } from 'ui/src/components/tool-tip'
 
 import * as styles from './styles.css'
-
-const messages = defineMessages({
-	close: {
-		id: 'rbrahO',
-		defaultMessage: 'Close',
-	},
-})
 
 interface IProps {
 	open: boolean
@@ -27,8 +19,6 @@ interface IProps {
 
 export const SlideOutDialog: React.FC<IProps> = props => {
 	const { children, open, headerButtons, onClose } = props
-
-	const intl = useIntl()
 
 	const navigateBack = () => {
 		onClose()
@@ -55,11 +45,11 @@ export const SlideOutDialog: React.FC<IProps> = props => {
 						<Box flexGrow={1} />
 						<Box flexGrow={1} display="flex" justifyContent="flex-end" gap="small">
 							{headerButtons}
-							<ToolTip message={intl.formatMessage(messages.close)}>
+							<DialogClose asChild>
 								<Button styleVariant="ghost" sizeVariant="small" iconOnly onClick={navigateBack}>
 									<Close2Icon />
 								</Button>
-							</ToolTip>
+							</DialogClose>
 						</Box>
 					</Box>
 					<ScrollArea className={styles.transactionScrollWrapper}>{children}</ScrollArea>
