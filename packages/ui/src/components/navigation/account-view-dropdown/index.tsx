cdb302ef892177eeac1dd0e5a61207f76b6eb84e
Heath Snee <heath.snee@gmail.com>
2023-10-30 18:25:44 +1100

    Use radix icon

--- packages/ui/src/components/navigation/account-view-dropdown/index.tsx
+++ packages/ui/src/components/navigation/account-view-dropdown/index.tsx
@@ -30,7 +30,7 @@ import {
 	MoonIcon,
 	PlusIcon,
 	ShareIcon,
-	SmartPhoneIcon,
+	RadixIcon,
 	SunIcon,
 	Z3usIcon,
 } from 'ui/src/components/icons'
@@ -176,7 +176,7 @@ export const AccountViewDropdown = forwardRef<HTMLElement, IAccountViewDropdownP
 												<DropdownMenuSub>
 													<DropdownMenuSubTrigger>
 														<DropdownMenuLeftSlot>
-															{keystore.type === KeystoreType.RADIX_WALLET && <SmartPhoneIcon />}
+															{keystore.type === KeystoreType.RADIX_WALLET && <RadixIcon />}
 															{keystore.type === KeystoreType.HARDWARE && <HardwareWalletIcon />}
 															{keystore.type === KeystoreType.LOCAL && <Z3usIcon />}
 														</DropdownMenuLeftSlot>
@@ -201,7 +201,7 @@ export const AccountViewDropdown = forwardRef<HTMLElement, IAccountViewDropdownP
 																	.map(k => (
 																		<DropdownMenuRadioItem key={k.id} value={k.id}>
 																			<DropdownMenuLeftSlot>
-																				{k.type === KeystoreType.RADIX_WALLET && <SmartPhoneIcon />}
+																				{k.type === KeystoreType.RADIX_WALLET && <RadixIcon />}
 																				{k.type === KeystoreType.HARDWARE && <HardwareWalletIcon />}
 																				{k.type === KeystoreType.LOCAL && <Z3usIcon />}
 																			</DropdownMenuLeftSlot>
