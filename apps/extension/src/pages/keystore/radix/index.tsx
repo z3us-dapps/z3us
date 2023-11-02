24a837eb5bec30b3ddcda34cae514e13a144a88c
Zac Godinez <zac.godinez@tutanota.com>
2023-11-03 09:48:06 +1100

    style: improve breadcrumb styles

--- apps/extension/src/pages/keystore/radix/index.tsx
+++ apps/extension/src/pages/keystore/radix/index.tsx
@@ -1,8 +1,11 @@
 // import { Paring } from '@radixdlt/connector-extension/src/pairing/pairing'
 import React, { useEffect, useState } from 'react'
+import { defineMessages, useIntl } from 'react-intl'
 import { useNavigate } from 'react-router-dom'
 import browser from 'webextension-polyfill'
 
+import { Box } from 'ui/src/components/box'
+import { Text } from 'ui/src/components/typography'
 import { KeystoreType } from 'ui/src/store/types'
 
 import type { Data } from '@src/types/vault'
@@ -11,8 +14,21 @@ import { DataType } from '@src/types/vault'
 import Done from '../components/done'
 import KeystoreForm from '../components/keystore-form'
 import { PASSWORD_STORAGE_KEY, Pairing, PairingState } from './components/pairing'
+import * as styles from './styles.css'
+
+const messages = defineMessages({
+	hardware_wallet_complete_title: {
+		defaultMessage: 'Create a new wallet',
+		id: 'wx278L',
+	},
+	hardware_wallet_complete_sub_title: {
+		defaultMessage: 'The password will be used to unlock your wallet.',
+		id: 'a4CP1S',
+	},
+})
 
 export const Radix: React.FC = () => {
+	const intl = useIntl()
 	const navigate = useNavigate()
 
 	const [pairingState, setPairingState] = useState<PairingState>(PairingState.LOADING)
@@ -37,8 +53,19 @@ export const Radix: React.FC = () => {
 	switch (step) {
 		case 2:
 			return <Done onNext={handleDone} />
+
 		case 1:
-			return <KeystoreForm keystoreType={KeystoreType.RADIX_WALLET} onSubmit={handleSubmit} onNext={() => setStep(2)} />
+			return (
+				<Box className={styles.pairingWrapper}>
+					<Box className={styles.pairingTextWrapper}>
+						<Text size="xxlarge" weight="strong" color="strong">
+							{intl.formatMessage(messages.hardware_wallet_complete_title)}
+						</Text>
+						<Text>{intl.formatMessage(messages.hardware_wallet_complete_sub_title)}</Text>
+					</Box>
+					<KeystoreForm keystoreType={KeystoreType.RADIX_WALLET} onSubmit={handleSubmit} onNext={() => setStep(2)} />
+				</Box>
+			)
 		default:
 			return (
 				<Pairing
--- packages/ui/src/pages/accounts/components/layout/components/breadcrumbs/index.tsx
+++ packages/ui/src/pages/accounts/components/layout/components/breadcrumbs/index.tsx
@@ -10,12 +10,14 @@ export const Breadcrumbs: React.FC = () => {
 	const validCrumbs = matches.filter(match => Boolean((match.handle as any)?.crumb))
 
 	return (
-		<Box className={styles.accountBreadCrumbWrapper}>
-			{validCrumbs.map((match, idx) => (
-				<React.Fragment key={match.id}>
-					{(match.handle as any).crumb(match.params, idx === validCrumbs.length - 1)}
-				</React.Fragment>
-			))}
+		<Box className={styles.accountBreadCrumbContainerWrapper}>
+			<Box className={styles.accountBreadCrumbWrapper}>
+				{validCrumbs.map((match, idx) => (
+					<Box className={styles.accountBreadCrumb} key={match.id}>
+						{(match.handle as any).crumb(match.params, idx === validCrumbs.length - 1)}
+					</Box>
+				))}
+			</Box>
 		</Box>
 	)
 }
