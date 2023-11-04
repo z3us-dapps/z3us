fbe88aacf334f81de08ca2a568b7e98c0b0f75fb
Zac Godinez <zac.godinez@tutanota.com>
2023-11-04 17:59:19 +1100

    style: improved login screen

---
+++ apps/extension/src/components/layout/styles.css.ts
@@ -0,0 +1,113 @@
+import { style } from '@vanilla-extract/css'
+
+import { sprinkles } from 'ui/src/components/system/sprinkles.css'
+import { responsiveStyle } from 'ui/src/components/system/theme-utils'
+import { vars } from 'ui/src/components/system/theme.css'
+
+export const unlockOuterWrapper = style([
+	sprinkles({
+		position: 'relative',
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 'vh100',
+		width: 'full',
+	}),
+	{},
+	responsiveStyle({
+		mobile: { backgroundColor: vars.color.backgroundSecondary },
+		tablet: { backgroundColor: vars.color.backgroundPrimary },
+	}),
+])
+
+export const unlockZ3usLogoWrapper = style([
+	sprinkles({
+		position: 'relative',
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		paddingBottom: {
+			mobile: 'none',
+			tablet: 'xlarge',
+		},
+	}),
+	{},
+])
+
+export const unlockInnerWrapper = style([
+	sprinkles({
+		position: 'relative',
+		width: 'full',
+	}),
+	{},
+	responsiveStyle({
+		tablet: {
+			borderRadius: vars.border.radius.xlarge,
+			background: vars.color.backgroundSecondary,
+			boxShadow: vars.color.shadowPanel,
+			maxWidth: '560px',
+		},
+		desktop: {},
+	}),
+])
+
+export const unlockPaddingWrapper = style([
+	sprinkles({
+		width: 'full',
+		paddingTop: {
+			mobile: 'xlarge',
+			tablet: 'xlarge',
+		},
+		paddingBottom: {
+			tablet: 'xlarge',
+		},
+		paddingX: {
+			mobile: 'xlarge',
+			tablet: 'xlarge',
+		},
+	}),
+	{},
+])
+
+export const unlockFormWrapper = style([
+	sprinkles({
+		display: 'flex',
+		flexDirection: 'column',
+	}),
+	{},
+])
+
+export const unlockFormTextWrapper = style([
+	sprinkles({
+		display: 'flex',
+		flexDirection: 'column',
+		gap: 'small',
+		paddingTop: {
+			mobile: 'large',
+		},
+	}),
+	{},
+])
+
+export const unlockFormWalletWrapper = style([
+	sprinkles({
+		display: 'flex',
+		flexDirection: 'column',
+		gap: 'small',
+		paddingTop: {
+			mobile: 'large',
+			tablet: 'large',
+		},
+	}),
+	{},
+])
+
+export const unlockValidationWrapper = style([
+	sprinkles({
+		paddingY: {
+			mobile: 'medium',
+		},
+	}),
+	{},
+])
--- apps/extension/src/pages/keystore/components/nav.tsx
+++ apps/extension/src/pages/keystore/components/nav.tsx
@@ -1,37 +1,16 @@
 import React from 'react'
-import { defineMessages, useIntl } from 'react-intl'
-import { useNavigate } from 'react-router-dom'
 
 import { Box } from 'ui/src/components/box'
-import { Button } from 'ui/src/components/button'
 import { Z3usLogoText } from 'ui/src/components/z3us-logo-babylon'
 
 import * as styles from './styles.css'
 
-const messages = defineMessages({
-	back: {
-		id: 'cyR7Kh',
-		defaultMessage: 'Back',
-	},
-})
-
-const Nav: React.FC = () => {
-	const intl = useIntl()
-	const navigate = useNavigate()
-
-	return (
-		<Box className={styles.onboardingNavWrapper}>
-			<Box display="flex" position="relative">
-				<Z3usLogoText />
-				{/* TODO: remove temp button */}
-				<Box style={{ position: 'absolute', top: '-5px', left: '-75px', opacity: 0 }}>
-					<Button onClick={() => navigate(-1)} styleVariant="ghost" sizeVariant="small">
-						{intl.formatMessage(messages.back)}
-					</Button>
-				</Box>
-			</Box>
+const Nav: React.FC = () => (
+	<Box className={styles.onboardingNavWrapper}>
+		<Box display="flex" position="relative">
+			<Z3usLogoText />
 		</Box>
-	)
-}
+	</Box>
+)
 
 export default Nav
