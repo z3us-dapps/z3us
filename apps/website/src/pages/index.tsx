b3063a6fa2ecd67c2aa580668f6de4cb93a29ab2
Zac Godinez <zac.godinez@tutanota.com>
2023-11-12 11:41:39 +1100

    feat: added favicon and default OG image

--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -1,23 +1,14 @@
 import { IndexPageTest } from '@/components/layouts/index-page'
-// import { IndexPage, IndexPageTest } from '@/components/layouts/index-page';
-import Head from 'next/head'
+import { Meta } from '@/components/meta'
 import React from 'react'
 
 const App = () => (
 	<>
-		<Head>
-			{/* TODO: favicon z3us.com */}
-			{/* TODO: meta description for z3us.com */}
-			<title>iPhone 12 XS Max For Sale in Colorado - Big Discounts | Apple</title>
-			<meta
-				name="description"
-				content="Check out iPhone 12 XR Pro and iPhone 12 Pro Max. Visit your local store and for expert advice."
-				key="home-page"
-			/>
-			<meta property="og:description" content="And a social description for our cool page" />
-			<meta property="og:image" content="https://example.com/images/cool-page.jpg" />
-		</Head>
-		{/* <IndexPage /> */}
+		<Meta
+			title="Z3US: An open source UX driven wallet for Radix DLT"
+			description="A community centered open source browser wallet for the Radix DLT network."
+			slug=""
+		/>
 		<IndexPageTest />
 	</>
 )
