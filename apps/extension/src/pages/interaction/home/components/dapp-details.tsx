80bac3ad877a5231ee7427c5227ae2db8c416999
Heath Snee <heath.snee@gmail.com>
2023-11-16 14:55:10 +1100

    Add verified dapp icon

--- apps/extension/src/pages/interaction/home/components/dapp-details.tsx
+++ apps/extension/src/pages/interaction/home/components/dapp-details.tsx
@@ -1,5 +1,7 @@
+import { CheckCircleIcon } from 'packages/ui/src/components/icons'
+import { ToolTip } from 'packages/ui/src/components/tool-tip'
 import { ValidationErrorMessage } from 'packages/ui/src/components/validation-error-message'
-import React from 'react'
+import React, { useMemo } from 'react'
 import { defineMessages, useIntl } from 'react-intl'
 import browser from 'webextension-polyfill'
 
@@ -10,11 +12,17 @@ import { useEntityMetadata } from 'ui/src/hooks/dapp/use-entity-metadata'
 import { findMetadataValue } from 'ui/src/services/metadata'
 import { getShortAddress } from 'ui/src/utils/string-utils'
 
+import matches from '@src/browser/manifest/content_matches.json'
+
 const messages = defineMessages({
 	origin_warning: {
 		id: 'HrF57B',
 		defaultMessage:
 			'Please be extra careful, origin ({origin}) of dApp you are interacting with is not included within claimed websites.',
+	},
+	verified_dapp: {
+		id: 'q92DfT',
+		defaultMessage: 'Verified dApp',
 	},
 })
 
@@ -34,12 +42,26 @@ export const DappDetails: React.FC<IProps> = ({ dAppDefinitionAddress, origin })
 	const infoUrl = findMetadataValue('info_url', data)
 	const description = findMetadataValue('description', data)
 
+	const isVerified = useMemo(
+		() =>
+			origin === popupURL.origin ||
+			!!matches.find(match => match.replace('*://*.', '').replace('/*', '').endsWith(new URL(origin).hostname)),
+		[origin],
+	)
+
 	return (
 		<Box display="flex" flexDirection="column" gap="xsmall" alignItems="center" justifyContent="center">
 			<Link color="strong" size="xlarge" weight="strong" href={infoUrl} underline="never" target="_blank">
-				<Box display="flex" flexDirection="column" gap="xsmall" alignItems="center" justifyContent="center">
+				<Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
 					<ResourceImageIcon size="xxlarge" address={dAppDefinitionAddress} />
-					{name || getShortAddress(dAppDefinitionAddress)}
+					<Box display="flex" flexDirection="row" alignItems="center" justifyContent="center">
+						{name || getShortAddress(dAppDefinitionAddress)}
+						{isVerified && (
+							<ToolTip message={intl.formatMessage(messages.verified_dapp)} side="top">
+								<CheckCircleIcon color="green" />
+							</ToolTip>
+						)}
+					</Box>
 				</Box>
 			</Link>
 
