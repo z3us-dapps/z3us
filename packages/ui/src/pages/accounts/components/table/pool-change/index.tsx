ca5f77c2c8e2bc8348c1c7abf14d835bcf6f4b3c
Heath Snee <heath.snee@gmail.com>
2023-11-27 20:02:03 +1100

    Fix pool change value

--- packages/ui/src/pages/accounts/components/table/pool-change/index.tsx
+++ packages/ui/src/pages/accounts/components/table/pool-change/index.tsx
@@ -1,6 +1,12 @@
-import type { FungibleResourcesCollectionItemVaultAggregated } from '@radixdlt/babylon-gateway-api-sdk'
+import type {
+	FungibleResourcesCollectionItemVaultAggregated,
+	StateEntityDetailsResponseFungibleResourceDetails,
+} from '@radixdlt/babylon-gateway-api-sdk'
 import { decimal } from '@radixdlt/radix-engine-toolkit'
 import clsx from 'clsx'
+import { useXRDPriceOnDay } from 'packages/ui/src/hooks/queries/coingecko'
+import { useTokens } from 'packages/ui/src/hooks/queries/tokens'
+import { useNoneSharedStore } from 'packages/ui/src/hooks/use-store'
 import React, { useEffect, useMemo, useState } from 'react'
 import { useIntl } from 'react-intl'
 
@@ -22,9 +28,13 @@ export const PoolChangeCell: React.FC<IProps> = props => {
 	const {
 		row: { original },
 	} = props
-	const { pool } = original as ResourceBalance[ResourceBalanceType.FUNGIBLE]
+	const { pool, address } = original as ResourceBalance[ResourceBalanceType.FUNGIBLE]
 
 	const intl = useIntl()
+
+	const { currency } = useNoneSharedStore(state => ({
+		currency: state.currency,
+	}))
 
 	const [before, setBefore] = useState<Date>(new Date())
 	useEffect(() => {
@@ -33,41 +43,64 @@ export const PoolChangeCell: React.FC<IProps> = props => {
 		setBefore(before)
 	}, [])
 
-	const { data: poolDataNow, isLoading: isLoadingNow } = useEntityDetails(pool)
-	const { data: poolDataBefore, isLoading: isLoadingBefore } = useEntityDetails(pool, undefined, undefined, before)
+	const { data: tokens, isLoading: isLoadingTokens } = useTokens()
 
-	const totalValueNow = useMemo(
-		() =>
-			poolDataNow?.fungible_resources.items.reduce(
-				(total, { vaults }: FungibleResourcesCollectionItemVaultAggregated) =>
-					total.add(vaults.items.reduce((sum, { amount: vaultAmount }) => sum.add(decimal(vaultAmount).value), ZERO)),
-				ZERO,
-			),
-		[poolDataNow],
+	const { data: xrdPriceNow = 0 } = useXRDPriceOnDay(currency, new Date())
+	const { data: xrdPriceBefore = 0 } = useXRDPriceOnDay(currency, before)
+
+	const { data: tokenDataNow, isLoading: isLoadingTokenNow } = useEntityDetails(address)
+	const { data: tokenDataBefore, isLoading: isLoadingTokenBefore } = useEntityDetails(
+		address,
+		undefined,
+		undefined,
+		before,
 	)
 
-	const totalValueBefore = useMemo(
-		() =>
-			poolDataBefore?.fungible_resources.items.reduce(
-				(total, { vaults }: FungibleResourcesCollectionItemVaultAggregated) =>
-					total.add(vaults.items.reduce((sum, { amount: vaultAmount }) => sum.add(decimal(vaultAmount).value), ZERO)),
+	const { data: poolDataNow, isLoading: isLoadingPoolNow } = useEntityDetails(pool)
+	const { data: poolDataBefore, isLoading: isLoadingPoolBefore } = useEntityDetails(pool, undefined, undefined, before)
+
+	const totalValueNow = useMemo(() => {
+		if (!tokenDataNow || !poolDataNow) return ZERO
+		return poolDataNow?.fungible_resources.items
+			.reduce(
+				(total, { resource_address, vaults }: FungibleResourcesCollectionItemVaultAggregated) =>
+					total.add(
+						vaults.items
+							.reduce((sum, { amount: vaultAmount }) => sum.add(decimal(vaultAmount).value), ZERO)
+							.mul(tokens[resource_address]?.price || 0),
+					),
 				ZERO,
-			),
-		[poolDataBefore],
-	)
+			)
+			.div((tokenDataNow?.details as StateEntityDetailsResponseFungibleResourceDetails)?.total_supply)
+	}, [poolDataNow, tokenDataNow, xrdPriceNow])
+
+	const totalValueBefore = useMemo(() => {
+		if (!tokenDataBefore || !poolDataBefore) return ZERO
+		return poolDataBefore?.fungible_resources.items
+			.reduce(
+				(total, { resource_address, vaults }: FungibleResourcesCollectionItemVaultAggregated) =>
+					total.add(
+						vaults.items
+							.reduce((sum, { amount: vaultAmount }) => sum.add(decimal(vaultAmount).value), ZERO)
+							.mul(tokens[resource_address]?.price || 0),
+					),
+				ZERO,
+			)
+			.div((tokenDataBefore?.details as StateEntityDetailsResponseFungibleResourceDetails)?.total_supply)
+	}, [poolDataBefore, tokenDataBefore, xrdPriceBefore])
 
 	const change = useMemo(
-		() =>
-			!totalValueNow || !totalValueBefore ? 0 : totalValueNow.sub(totalValueBefore).div(totalValueBefore).toNumber(),
+		() => (totalValueBefore.gt(0) ? totalValueNow.sub(totalValueBefore).div(totalValueBefore).toNumber() : 0),
 		[totalValueNow, totalValueBefore],
 	)
 
-	if (isLoadingNow || isLoadingBefore) return <FallbackLoading />
+	if (isLoadingTokens || isLoadingTokenNow || isLoadingTokenBefore || isLoadingPoolNow || isLoadingPoolBefore)
+		return <FallbackLoading />
 
 	return (
 		<Box className={styles.assetStatisticCellWrapper}>
 			<Box className={clsx(styles.assetStatisticCellContentWrapper, 'td-cell')}>
-				<Text size="small" color={change && change > 0 ? 'green' : 'red'} truncate>
+				<Text size="small" color={change > 0 ? 'green' : 'red'} truncate>
 					{intl.formatNumber(change, {
 						style: 'percent',
 						maximumFractionDigits: 2,
