3668f495f6d2fa252a384a640719c315ed9269f1
Heath Snee <heath.snee@gmail.com>
2024-02-29 22:43:44 +1100

    Add third party deposit settings

--- packages/ui/src/pages/settings/accounts/index.tsx
+++ packages/ui/src/pages/settings/accounts/index.tsx
@@ -1,5 +1,9 @@
+import { DefaultDepositRule } from '@radixdlt/radix-engine-toolkit'
+import { useQueryClient } from '@tanstack/react-query'
 import React, { useEffect, useState } from 'react'
 import { defineMessages, useIntl } from 'react-intl'
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom'
+import { toast } from 'sonner'
 
 import { AccountCards } from 'ui/src/components/account-cards'
 import { Avatar } from 'ui/src/components/avatar'
@@ -9,8 +13,10 @@ import { SelectAdapter as AccountSelect } from 'ui/src/components/form/fields/ac
 import { Input } from 'ui/src/components/input'
 import { Text } from 'ui/src/components/typography'
 import { CARD_COLORS, CARD_IMAGES } from 'ui/src/constants/account'
+import { useEntityDetails } from 'ui/src/hooks/dapp/use-entity-details'
 import { useNetworkId } from 'ui/src/hooks/dapp/use-network'
 import { useWalletAccounts } from 'ui/src/hooks/use-accounts'
+import { useSendTransaction } from 'ui/src/hooks/use-send-transaction'
 import { useNoneSharedStore } from 'ui/src/hooks/use-store'
 import type { AddressBookEntry } from 'ui/src/store/types'
 
@@ -48,14 +54,64 @@ const messages = defineMessages({
 		id: 'yWx/0A',
 		defaultMessage: 'Account image',
 	},
+	third_party_deposits: {
+		id: '93MRNX',
+		defaultMessage: 'Third party deposits',
+	},
+	third_party_deposits_accept_all_title: {
+		id: 'wLLWDX',
+		defaultMessage: 'Accept all deposits',
+	},
+	third_party_deposits_accept_all_description: {
+		id: 'z0CNtW',
+		defaultMessage: 'Allow third-parties to deposit any asset',
+	},
+	third_party_deposits_accept_known_title: {
+		id: 'deE3OR',
+		defaultMessage: 'Only accept known',
+	},
+	third_party_deposits_accept_known_description: {
+		id: '4jkBhr',
+		defaultMessage: 'Allow third-parties to deposit only assets this Account already holds',
+	},
+	third_party_deposits_deny_all_title: {
+		id: 'yc2VjA',
+		defaultMessage: 'Deny all',
+	},
+	third_party_deposits_deny_all_description: {
+		id: 'zlB8/W',
+		defaultMessage: 'Deny all third-party deposits',
+	},
+	error_toast: {
+		id: 'fjqZcw',
+		defaultMessage: 'Failed submitting transaction to the network',
+	},
+	success_toast: {
+		id: 'Gkt0d0',
+		defaultMessage: 'Successfully submitted transaction to the network',
+	},
+	toast_action_label: {
+		id: 'K7AkdL',
+		defaultMessage: 'Show',
+	},
 })
 
 const Accounts: React.FC = () => {
 	const intl = useIntl()
+	const queryClient = useQueryClient()
+	const location = useLocation()
+	const navigate = useNavigate()
+	const [searchParams] = useSearchParams()
 	const networkId = useNetworkId()
 	const accounts = useWalletAccounts()
+	const sendTransaction = useSendTransaction()
 	const accountsAsArray = Object.values(accounts)
+
+	const [currentRule, setRule] = useState<DefaultDepositRule | undefined>()
 	const [selectedAccount, setSelectedAccount] = useState<AddressBookEntry | undefined>()
+
+	const { data } = useEntityDetails(selectedAccount?.address)
+
 	const { setAddressBookEntry } = useNoneSharedStore(state => ({
 		setAddressBookEntry: state.setAddressBookEntryAction,
 	}))
@@ -66,6 +122,12 @@ const Accounts: React.FC = () => {
 			setSelectedAccount(accountsAsArray[0])
 		}
 	}, [accounts])
+
+	useEffect(() => {
+		const rule = (data?.details as any)?.state.default_deposit_rule
+		if (!rule) return
+		setRule(rule)
+	}, [(data?.details as any)?.state.default_deposit_rule])
 
 	const handleSelectAccount = (address: string) => {
 		setSelectedAccount(accountsAsArray.find(a => a.address === address))
@@ -94,6 +156,41 @@ const Accounts: React.FC = () => {
 		const entry = { ...selectedAccount, name: event.target.value }
 		setAddressBookEntry(networkId, selectedAccount.address, entry)
 		setSelectedAccount(entry)
+	}
+
+	// https://github.com/radixdlt/radixdlt-scrypto/blob/develop/transaction/examples/account/deposit_modes.rtm
+	const handleThirdPartyDeposits = async (newRule: DefaultDepositRule) => {
+		setRule(undefined)
+
+		await sendTransaction({
+			version: 1,
+			transactionManifest: `
+			CALL_METHOD 
+				Address("${data.address}") 
+				"set_default_deposit_rule" 
+				Enum<DefaultDepositRule::${newRule}>();
+		`,
+		})
+			.then(value => {
+				setRule(newRule)
+				queryClient.invalidateQueries({ queryKey: ['useEntitiesDetails', networkId, [data.address]] })
+				toast.success(intl.formatMessage(messages.success_toast), {
+					description: value.status,
+					action: {
+						label: intl.formatMessage(messages.toast_action_label),
+						onClick: () => {
+							searchParams.set('tx', `${value.transactionIntentHash}`)
+							navigate(`${location.pathname}?${searchParams}`)
+						},
+					},
+				})
+			})
+			.catch(error => {
+				setRule((data?.details as any)?.state.default_deposit_rule)
+				toast.error(intl.formatMessage(messages.error_toast), {
+					description: error.message || error.error,
+				})
+			})
 	}
 
 	return (
@@ -173,6 +270,59 @@ const Accounts: React.FC = () => {
 										/>
 									</Button>
 								))}
+							</Box>
+						</Box>
+						<Box display="flex" flexDirection="column" gap="small">
+							<Text size="small" weight="medium" color="strong">
+								{intl.formatMessage(messages.third_party_deposits)}
+							</Text>
+							<Box display="flex" gap="small" flexDirection="column" flexWrap="wrap" flexGrow={0} flexShrink={0}>
+								<Button
+									active={currentRule === DefaultDepositRule.Accept}
+									onClick={() => handleThirdPartyDeposits(DefaultDepositRule.Accept)}
+									disabled={!currentRule}
+									styleVariant="avatar"
+									sizeVariant="medium"
+								>
+									<Box width="full" height="full" borderRadius="full">
+										<Text size="small" weight="medium" color="strong">
+											{intl.formatMessage(messages.third_party_deposits_accept_all_title)}
+										</Text>
+										<Text size="xxsmall">
+											{intl.formatMessage(messages.third_party_deposits_accept_all_description)}
+										</Text>
+									</Box>
+								</Button>
+								<Button
+									active={currentRule === DefaultDepositRule.AllowExisting}
+									onClick={() => handleThirdPartyDeposits(DefaultDepositRule.AllowExisting)}
+									disabled={!currentRule}
+									styleVariant="avatar"
+									sizeVariant="medium"
+								>
+									<Box width="full" height="full" borderRadius="full">
+										<Text size="small" weight="medium" color="strong">
+											{intl.formatMessage(messages.third_party_deposits_accept_known_title)}
+										</Text>
+										<Text size="xxsmall">
+											{intl.formatMessage(messages.third_party_deposits_accept_known_description)}
+										</Text>
+									</Box>
+								</Button>
+								<Button
+									active={currentRule === DefaultDepositRule.Reject}
+									onClick={() => handleThirdPartyDeposits(DefaultDepositRule.Reject)}
+									disabled={!currentRule}
+									styleVariant="avatar"
+									sizeVariant="medium"
+								>
+									<Box width="full" height="full" borderRadius="full">
+										<Text size="small" weight="medium" color="strong">
+											{intl.formatMessage(messages.third_party_deposits_deny_all_title)}
+										</Text>
+										<Text size="xxsmall">{intl.formatMessage(messages.third_party_deposits_deny_all_description)}</Text>
+									</Box>
+								</Button>
 							</Box>
 						</Box>
 					</Box>
