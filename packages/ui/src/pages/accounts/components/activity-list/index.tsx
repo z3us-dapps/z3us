ac91a1302096222eaffb8663335e57a4bef6ad67
Heath Snee <heath.snee@gmail.com>
2024-05-10 19:16:58 +1000

    Do not render chart on mobile

--- packages/ui/src/pages/accounts/components/activity-list/index.tsx
+++ packages/ui/src/pages/accounts/components/activity-list/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import { Box } from 'ui/src/components/box'
-import { useIsMobileWidth } from 'ui/src/hooks/use-is-mobile'
 
 import HorizontalAccountsScrollList from '../horizontal-accounts-scroll-list'
 import { AccountButtons } from './components/account-buttons'
@@ -11,21 +10,17 @@ import { SideBarAccountCard } from './components/side-bar-account-card'
 import { SideBarTotal } from './components/side-bar-total'
 import * as styles from './styles.css'
 
-const Sidebar: React.FC = () => {
-	const isMobile = useIsMobileWidth()
-
-	return (
-		<Box className={styles.sideBarWrapper}>
-			{!isMobile && <BalancePieChart />}
-			<SideBarTotal />
-			<SideBarAccountCard />
-			<Box display={['block', 'none']}>
-				<HorizontalAccountsScrollList />
-			</Box>
-			<AccountButtons />
-			<ActivityList />
+const Sidebar: React.FC = () => (
+	<Box className={styles.sideBarWrapper}>
+		<BalancePieChart />
+		<SideBarTotal />
+		<SideBarAccountCard />
+		<Box display={['block', 'none']}>
+			<HorizontalAccountsScrollList />
 		</Box>
-	)
-}
+		<AccountButtons />
+		<ActivityList />
+	</Box>
+)
 
 export default Sidebar
