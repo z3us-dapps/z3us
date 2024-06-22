9943a8a789a57b16438707e3d0460f5a69ecd7e2
Zac Godinez <zac.godinez@tutanota.com>
2024-06-22 21:02:27 +1000

    style: work on nft styles for mobile

--- packages/ui/src/pages/accounts/components/sidebar/components/side-bar-account-card/index.tsx
+++ packages/ui/src/pages/accounts/components/sidebar/components/side-bar-account-card/index.tsx
@@ -3,18 +3,21 @@ import { useParams } from 'react-router-dom'
 
 import { AccountCard } from 'ui/src/components/account-cards'
 import { Box } from 'ui/src/components/box'
+import { useIsAllAccounts } from 'ui/src/hooks/use-is-all-accounts'
 
 import * as styles from './styles.css'
 
 export const SideBarAccountCard: React.FC = () => {
 	const { accountId = '-' } = useParams()
 
+	const isAllAccounts = useIsAllAccounts()
+
 	if (accountId === '-') return null
 
 	return (
 		<Box className={styles.mobileCardWrapper}>
 			<Box className={styles.mobileCardTransparentWrapper}>
-				<AccountCard address={accountId} showCopyAddressButton={false} />
+				<AccountCard address={accountId} showCopyAddressButton={false} isTransparent={!isAllAccounts} />
 			</Box>
 		</Box>
 	)
