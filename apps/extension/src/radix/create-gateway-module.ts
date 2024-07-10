2bba33bbc755fc32522a333e84505a37eeade37e
Heath Snee <heath.snee@gmail.com>
2024-07-10 18:51:19 +0200

    Update gateway module details

--- apps/extension/src/radix/create-gateway-module.ts
+++ apps/extension/src/radix/create-gateway-module.ts
@@ -1,6 +1,9 @@
 import { RadixNetworkConfigById } from '@radixdlt/babylon-gateway-api-sdk'
-import { __VERSION__ } from '@radixdlt/connector-extension/src/version'
 import { GatewayModule } from '@radixdlt/radix-dapp-toolkit'
+
+import { DAPP_ADDRESS, DAPP_NAME } from 'ui/src/constants/dapp'
+
+import { config } from '@src/config'
 
 // import createGatewayModule from '@radixdlt/connector-extension/src/chrome/background/create-gateway-module'
 
@@ -8,8 +11,8 @@ export const createGatewayModule = (networkId: number): GatewayModule =>
 	GatewayModule({
 		clientConfig: {
 			basePath: RadixNetworkConfigById[networkId].gatewayUrl,
-			applicationName: 'Radix Connector Extension',
-			applicationVersion: __VERSION__,
-			applicationDappDefinitionAddress: '',
+			applicationName: DAPP_NAME,
+			applicationVersion: config.version,
+			applicationDappDefinitionAddress: DAPP_ADDRESS,
 		},
 	})
