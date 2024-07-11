faa51390a8e8bc121195c11a2b576470ac2a46a4
Heath Snee <heath.snee@gmail.com>
2024-07-11 22:40:54 +0200

    Add neverthrow to paths

--- apps/extension/src/vite-env.d.ts
+++ apps/extension/src/vite-env.d.ts
@@ -1,2 +1,3 @@
 /// <reference types="vite/client" />
 /// <reference types="rollup-plugin-chrome-extension/client" />
+/// <reference types="@radixdlt/connector-extension/node_modules/@radixdlt/radix-dapp-toolkit/node_modules/neverthrow" />
